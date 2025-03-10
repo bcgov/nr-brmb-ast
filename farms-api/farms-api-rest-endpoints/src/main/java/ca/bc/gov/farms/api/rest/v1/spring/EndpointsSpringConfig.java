da4b4ec77ec512fedbb2092f98daf4eb26b2fef1
Hong-Yi Wang <hwang@vividsolutions.com>
2025-03-10 14:12:15 -0700

    AS-2921: Implement EndpointsSpringConfig.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
@@ -1,8 +1,16 @@
 package ca.bc.gov.farms.api.rest.v1.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import ca.bc.gov.nrs.wfone.common.checkhealth.CheckHealthValidator;
+import ca.bc.gov.nrs.wfone.common.checkhealth.CompositeValidator;
+import ca.bc.gov.nrs.wfone.common.utils.ApplicationContextProvider;
 
 @Configuration
 public class EndpointsSpringConfig {
@@ -13,5 +21,29 @@ public class EndpointsSpringConfig {
         logger.info("<EndpointsSpringConfig");
 
         logger.info(">EndpointsSpringConfig");
+    }
+
+    @Bean
+    public ApplicationContextProvider applicationContextProvider() {
+        return new ApplicationContextProvider();
+    }
+
+    @Bean(initMethod = "init")
+    public CompositeValidator checkHealthValidator() {
+        CompositeValidator result;
+
+        result = new CompositeValidator();
+        result.setComponentIdentifier("FARMS_REST");
+        result.setComponentName("Farmer Access to Risk Management Service V1 Rest API");
+        result.setValidators(healthCheckValidators());
+
+        return result;
+    }
+
+    @Bean
+    public List<CheckHealthValidator> healthCheckValidators() {
+        List<CheckHealthValidator> result = new ArrayList<>();
+
+        return result;
     }
 }
