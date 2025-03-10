ffbadcaddb628a03f650d41a4d830147301f3e96
Hong-Yi Wang <hwang@vividsolutions.com>
2025-03-10 10:34:05 -0700

    AS-2921: Initial commit for farm-api-rest-endpoints.

---
+++ farm-api/farm-api-rest-endpoints/src/test/java/ca/bc/gov/mal/farm/api/FarmApiRestEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.mal.farm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmApiRestEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
