92e73eb4d77c13729b97ae5cf646b583e61b11d7
Hong-Yi Wang <hwang@vividsolutions.com>
2025-03-10 10:34:05 -0700

    AS-2921: Initial commit for farm-api-rest-endpoints.

---
+++ farm-api/farm-api-rest-endpoints/src/main/java/ca/bc/gov/mal/farm/api/FarmApiRestEndpointsApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.mal.farm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmApiRestEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmApiRestEndpointsApplication.class, args);
+	}
+
+}
---
+++ farm-api/farm-api-rest-endpoints/src/main/java/ca/bc/gov/mal/farm/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.mal.farm.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FarmApiRestEndpointsApplication.class);
+	}
+
+}
