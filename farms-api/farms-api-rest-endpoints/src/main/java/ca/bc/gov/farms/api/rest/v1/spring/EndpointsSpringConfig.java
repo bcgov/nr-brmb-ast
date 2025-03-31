727376bc61d712fc49eb2c9a6f117a0f9aaa4ec6
Hong-Yi Wang <hwang@vividsolutions.com>
2025-03-31 12:12:05 -0700

    AS-2921: Add Dockerfile.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
@@ -3,11 +3,15 @@ package ca.bc.gov.farms.api.rest.v1.spring;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import ca.bc.gov.farms.service.api.v1.spring.ServiceApiSpringConfig;
 import ca.bc.gov.nrs.wfone.common.checkhealth.CheckHealthValidator;
@@ -34,6 +38,27 @@ public class EndpointsSpringConfig {
     @Bean
     public ApplicationContextProvider applicationContextProvider() {
         return new ApplicationContextProvider();
+    }
+
+    @Primary
+    @Bean
+    public DataSource farmsDataSource() {
+        DataSource result;
+
+        final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+        dsLookup.setResourceRef(true);
+        result = dsLookup.getDataSource("java:comp/env/jdbc/farms_rest");
+
+        return result;
+    }
+
+    @Bean
+    public DataSource codeTableDataSource() {
+        DataSource result;
+
+        result = farmsDataSource();
+
+        return result;
     }
 
     @Bean(initMethod = "init")
