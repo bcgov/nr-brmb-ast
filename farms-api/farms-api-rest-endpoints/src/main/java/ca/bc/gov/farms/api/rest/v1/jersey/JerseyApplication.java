804f2aba017b9166e36219c917625a5e5309e79b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-04-01 09:01:06 -0700

    AS-2921: Add ApplicationInitializer.

---
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/jersey/JerseyApplication.java
@@ -0,0 +1,30 @@
+package ca.bc.gov.farms.api.rest.v1.jersey;
+
+import javax.servlet.ServletConfig;
+import javax.ws.rs.core.Context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.nrs.wfone.common.api.rest.code.endpoints.impl.CodeTableEndpointsImpl;
+import ca.bc.gov.nrs.wfone.common.api.rest.code.endpoints.impl.CodeTableListEndpointsImpl;
+import ca.bc.gov.nrs.wfone.common.rest.endpoints.jersey.JerseyResourceConfig;
+
+public class JerseyApplication extends JerseyResourceConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(JerseyApplication.class);
+
+    /**
+     * Register JAX-RS application components.
+     */
+    public JerseyApplication(@Context ServletConfig servletConfig) {
+        super();
+
+        logger.debug("<JerseyApplication");
+
+        register(CodeTableEndpointsImpl.class);
+        register(CodeTableListEndpointsImpl.class);
+
+        logger.debug(">JerseyApplication");
+    }
+}
