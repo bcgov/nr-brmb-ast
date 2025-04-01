a5d22fb38c824b1de6184a4d505514895436ef60
Hong-Yi Wang <hwang@vividsolutions.com>
2025-04-01 13:22:24 -0700

    AS-2921: Fix compilation errors.

--- farms-api/farms-api-rest-common/src/main/java/ca/bc/gov/farms/api/rest/v1/resource/types/ResourceTypes.java
+++ farms-api/farms-api-rest-common/src/main/java/ca/bc/gov/farms/api/rest/v1/resource/types/ResourceTypes.java
@@ -3,5 +3,10 @@ package ca.bc.gov.farms.api.rest.v1.resource.types;
 import ca.bc.gov.nrs.common.wfone.rest.resource.types.BaseResourceTypes;
 
 public class ResourceTypes extends BaseResourceTypes {
+	
+	public static final String NAMESPACE = "http://farms.gov.bc.ca/v1/";
+
+	public static final String ENDPOINTS_NAME = "endpoints";
+	public static final String ENDPOINTS = NAMESPACE + ENDPOINTS_NAME;
 
 }
