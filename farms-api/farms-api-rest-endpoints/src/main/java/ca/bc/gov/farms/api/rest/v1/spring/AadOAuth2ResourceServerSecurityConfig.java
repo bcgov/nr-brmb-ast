ecb24ce0d630e4204b81b4f65d440dcc23661810
Hong-Yi Wang <hwang@vividsolutions.com>
2025-04-03 15:00:08 -0700

    AS-3018: Modify AadOAuth2ResourceServerSecurityConfig.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/AadOAuth2ResourceServerSecurityConfig.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/AadOAuth2ResourceServerSecurityConfig.java
@@ -1,29 +1,20 @@
 package ca.bc.gov.farms.api.rest.v1.spring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import com.azure.spring.cloud.autoconfigure.aad.AadResourceServerWebSecurityConfigurerAdapter;
 
-import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadResourceServerHttpSecurityConfigurer;
-
-@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
-@EnableMethodSecurity
-public class AadOAuth2ResourceServerSecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AadOAuth2ResourceServerSecurityConfig extends AadResourceServerWebSecurityConfigurerAdapter {
 
     /**
      * Add configuration logic as needed.
      */
-    @Bean
-    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-        http.apply(AadResourceServerHttpSecurityConfigurer.aadResourceServer())
-                .and()
-                .authorizeHttpRequests()
-                .antMatchers("/checkHealth").permitAll()
-                .anyRequest().authenticated();
-        return http.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
     }
 }
