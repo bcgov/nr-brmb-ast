501a8f63a44f5b785e8313d2c18bf2a260b58ee0
Hong-Yi Wang <hwang@vividsolutions.com>
2025-04-07 11:31:31 -0700

    AS-3018: Fix the issue with "invalid_token".

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
-
 import ca.bc.gov.farms.service.api.v1.spring.ServiceApiSpringConfig;
 import ca.bc.gov.nrs.wfone.common.checkhealth.CheckHealthValidator;
 import ca.bc.gov.nrs.wfone.common.checkhealth.CompositeValidator;
@@ -24,6 +23,7 @@ import ca.bc.gov.nrs.wfone.common.utils.ApplicationContextProvider;
         PropertiesSpringConfig.class,
         ServiceApiSpringConfig.class,
         CodeEndpointsSpringConfig.class,
+        SecurityConfig.class,
         WebConfig.class,
         CorsFilter.class
 })
