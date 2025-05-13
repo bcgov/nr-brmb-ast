5c98b50fefed48701cf9027fb6d35dde15e6efd1
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-13 11:05:24 -0700

    AS-3092: Implement BenchmarkPerUnit.

---
+++ farms-api/farms-model/src/main/java/ca/bc/gov/farms/model/v1/BenchmarkPerUnit.java
@@ -0,0 +1,25 @@
+package ca.bc.gov.farms.model.v1;
+
+public interface BenchmarkPerUnit extends Serializable {
+
+    public Long getBenchmarkPerUnitId();
+    public void setBenchmarkPerUnitId(Long benchmarkPerUnitId);
+
+    public Integer getProgramYear();
+    public void setProgramYear(Integer programYear);
+
+    public String getUnitComment();
+    public void setUnitComment(String unitComment);
+
+    public Date getExpiryDate();
+    public void setExpiryDate(Date expiryDate);
+
+    public String getMunicipalityCode();
+    public void setMunicipalityCode(String municipalityCode);
+
+    public String getInventoryItemCode();
+    public void setInventoryItemCode(String inventoryItemCode);
+
+    public String getStructureGroupCode();
+    public void setStructureGroupCode(String structureGroupCode);
+}
