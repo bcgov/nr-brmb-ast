c8fbbc657d9fc19b3be72bfbdceb8b281b7065b6
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-14 14:40:21 -0700

    AS-3092: Use BRMB common library's CodeTableEndpoints and CodeTableListEndpoints.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/jersey/JerseyApplication.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/jersey/JerseyApplication.java
@@ -7,10 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ca.bc.gov.farms.api.rest.v1.endpoints.impl.BenchmarkPerUnitEndpointsImpl;
-import ca.bc.gov.farms.api.rest.v1.endpoints.impl.CodeTableEndpointsImpl;
-import ca.bc.gov.farms.api.rest.v1.endpoints.impl.CodeTableListEndpointsImpl;
 import ca.bc.gov.farms.api.rest.v1.endpoints.impl.TopLevelEndpointsImpl;
 import ca.bc.gov.brmb.common.api.rest.code.endpoints.impl.CodeEndpointsImpl;
+import ca.bc.gov.brmb.common.api.rest.code.endpoints.impl.CodeTableEndpointsImpl;
+import ca.bc.gov.brmb.common.api.rest.code.endpoints.impl.CodeTableListEndpointsImpl;
 import ca.bc.gov.brmb.common.rest.endpoints.jersey.JerseyResourceConfig;
 
 public class JerseyApplication extends JerseyResourceConfig {
