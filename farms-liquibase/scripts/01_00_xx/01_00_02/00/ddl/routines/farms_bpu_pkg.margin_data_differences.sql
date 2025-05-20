e13bab7f604045a9fee7af2844539c5d6c85d2e3
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-20 14:16:01 -0700

    AS-3112: Implement farms_bpu_pkg.margin_data_differences.

--- farms-liquibase/scripts/01_00_xx/01_00_02/00/ddl/routines/farms_bpu_pkg.margin_data_differences.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/00/ddl/routines/farms_bpu_pkg.margin_data_differences.sql
@@ -7,27 +7,31 @@ returns integer
 language plpgsql
 as $$
 declare
+    c_benchmark_year cursor (p_year numeric, p_margin numeric) for
+        select average_margin
+        from farms.benchmark_year
+        where benchmark_per_unit_id = in_benchmark_per_unit_id
+        and benchmark_year = p_year
+        and average_margin <> p_margin;
+
     v_number_differences integer := 0;
     v_margin numeric;
     v_year numeric;
-    v_exists boolean;
+    v_benchmark_year record;
 begin
     for ref_year_index in 1..6
     loop
         v_margin := in_margin_data[ref_year_index];
         v_year := in_program_year - ref_year_index;
 
-        select exists (
-            select 1
-            from farms.benchmark_year
-            where benchmark_per_unit_id = in_benchmark_per_unit_id
-            and benchmark_year = v_year
-            and average_margin <> v_margin
-        ) into v_exists;
+        open c_benchmark_year(v_year, v_margin);
+        fetch c_benchmark_year into v_benchmark_year;
 
-        if v_exists then
+        if found then
             v_number_differences := v_number_differences + 1;
         end if;
+
+        close c_benchmark_year;
     end loop;
 
     return v_number_differences;
