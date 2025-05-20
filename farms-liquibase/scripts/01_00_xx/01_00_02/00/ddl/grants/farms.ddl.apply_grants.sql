21f5280f0e655a52bde491b0cef8a2eec5ccb950
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-20 11:12:10 -0700

    AS-3112: Implement farms_bpu_pkg.validate_program_year.

--- farms-liquibase/scripts/01_00_xx/01_00_02/00/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/00/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,2 +1,3 @@
 GRANT EXECUTE ON FUNCTION farms_bpu_pkg.is_group_code(VARCHAR) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.insert_error(NUMERIC, NUMERIC, VARCHAR) TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.validate_program_year(NUMERIC) TO "app_farms_rest_proxy";
