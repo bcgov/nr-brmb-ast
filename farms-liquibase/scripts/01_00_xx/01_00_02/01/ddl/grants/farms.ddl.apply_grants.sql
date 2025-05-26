2673f5ec7b7e6f24b90514c23494de83b8fff0e5
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-26 09:36:10 -0700

    AS-3107: Create farms_import_pkg.numbers_equal.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,5 @@
+GRANT EXECUTE ON FUNCTION farms_import_pkg.numbers_equal(NUMERIC, NUMERIC) TO "app_farms_rest_proxy";
+
 GRANT EXECUTE ON FUNCTION farms_version_pkg.create_version(
     farms.import_version.description%TYPE,
     farms.import_version.import_file_name%TYPE,
