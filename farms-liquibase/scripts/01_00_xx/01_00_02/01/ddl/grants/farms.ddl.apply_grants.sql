a4d08f50230d73482c54bc3746458130a23e5c4a
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-26 09:12:14 -0700

    AS-3107: Create farms_version_pkg.start_import.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -7,3 +7,4 @@ GRANT EXECUTE ON PROCEDURE farms_version_pkg.start_upload(NUMERIC, VARCHAR) TO "
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.update_control_file_info_stg(NUMERIC, VARCHAR) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_version_pkg.uploaded_version(NUMERIC, VARCHAR, VARCHAR) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.upload_failure(NUMERIC, VARCHAR, VARCHAR) TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_version_pkg.start_import(NUMERIC, VARCHAR) TO "app_farms_rest_proxy";
