bb490ffcf975c830202eaa8055549eb27e675acf
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-26 10:37:50 -0700

    AS-3107: Create farms_import_pkg.close_global_log.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_import_pkg.close_global_log.sql
@@ -0,0 +1,23 @@
+create or replace procedure farms_import_pkg.close_global_log(
+   in in_version_id numeric
+)
+language plpgsql
+as $$
+declare
+    g1_cursor cursor for
+        select t.log_message msg
+        from farms.import_log t
+        where t.import_version_id = in_version_id
+        order by t.import_log_id asc;
+begin
+    for g1_val in g1_cursor
+    loop
+        update farms.import_version
+        set import_audit_information = import_audit_information || g1_val.msg
+        where import_version_id = in_version_id;
+    end loop;
+
+    delete from farms.import_log
+    where import_version_id = in_version_id;
+end;
+$$;
