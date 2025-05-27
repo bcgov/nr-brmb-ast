dc37f5fbe0feb3cec1e43b76637008f482dde946
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-27 10:31:23 -0700

    AS-3107: Create farms_import_pkg.commodities.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -75,6 +75,7 @@ GRANT EXECUTE ON PROCEDURE farms_import_pkg.clear_log(NUMERIC) TO "app_farms_res
 GRANT EXECUTE ON PROCEDURE farms_import_pkg.close_global_log(NUMERIC) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_import_pkg.scrub(VARCHAR) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_import_pkg.production_unit(NUMERIC, VARCHAR) TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_import_pkg.commodities(NUMERIC, VARCHAR) TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_version_pkg.create_version(
     farms.import_version.description%TYPE,
