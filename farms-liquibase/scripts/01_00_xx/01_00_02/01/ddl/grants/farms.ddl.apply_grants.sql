7d5dd323d8e87264762e6588f9518ea399db0ebf
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-28 11:38:24 -0700

    AS-3107: Create farms_import_pkg.reported_inventory.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -30,6 +30,7 @@ GRANT EXECUTE ON FUNCTION farms_import_pkg.strings_are_different TO "app_farms_r
 GRANT EXECUTE ON FUNCTION farms_import_pkg.person TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_import_pkg.participant TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_import_pkg.setup_unknown_codes TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_import_pkg.reported_inventory TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_version_pkg.create_version TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.start_upload TO "app_farms_rest_proxy";
