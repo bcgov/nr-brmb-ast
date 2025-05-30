6dbaf795c15a214658aa43fb14d2fbf89a85889b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-30 10:30:04 -0700

    AS-3107: Create farms_import_pkg.is_changed.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_import_pkg.is_changed.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_import_pkg.is_changed.sql
@@ -1,4 +1,4 @@
-create or replace function farms_import_pkg.scrub(
+create or replace function farms_import_pkg.is_changed(
     in in_program_year_id farms.program_year.program_year_id%type,
     in in_program_year_vrsn_prev_id farms.program_year_version.program_year_version_id%type
 )
