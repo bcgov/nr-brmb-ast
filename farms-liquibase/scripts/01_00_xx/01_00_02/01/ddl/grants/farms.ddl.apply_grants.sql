66da6062dd43e1030d5da8fe7814ab0b90dd3563
Hong-Yi Wang <hwang@vividsolutions.com>
2025-05-30 14:32:00 -0700

    AS-3107: Create farms_codes_write_pkg.expire_fmv.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -7,6 +7,7 @@ GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_line_item TO "app_farms_
 GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.copy_year_line_items TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.expire_fmv TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
