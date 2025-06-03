f93885366549218f7b584a459be1b68592f9bb56
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 09:05:36 -0700

    AS-3107: Create farms_codes_write_pkg.update_structure_group_code.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -42,6 +42,7 @@ GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_participant_prof_code TO
 GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_participant_prof_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_participant_prof_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_structure_group_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_structure_group_code TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
