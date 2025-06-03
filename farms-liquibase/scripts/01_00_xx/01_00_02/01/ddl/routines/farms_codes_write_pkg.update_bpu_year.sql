b81dac65029aac7a46b0dc01f5466f47150508d4
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 09:41:14 -0700

    AS-3107: Create farms_codes_write_pkg.update_bpu_year.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_codes_write_pkg.update_bpu_year.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_codes_write_pkg.update_bpu_year.sql
@@ -1,10 +1,10 @@
 create or replace procedure farms_codes_write_pkg.update_bpu_year(
-   in in_benchmark_per_unit_id farms.benchmark_per_unit_year.benchmark_per_unit_id%type,
-   in in_year farms.benchmark_per_unit_year.benchmark_year%type,
-   in in_margin farms.benchmark_per_unit_year.average_margin%type,
-   in in_expense farms.benchmark_per_unit_year.average_expense%type,
-   in in_revision_count farms.benchmark_per_unit_year.revision_count%type,
-   in in_user farms.benchmark_per_unit_year.update_user%type
+   in in_benchmark_per_unit_id farms.benchmark_year.benchmark_per_unit_id%type,
+   in in_year farms.benchmark_year.benchmark_year%type,
+   in in_margin farms.benchmark_year.average_margin%type,
+   in in_expense farms.benchmark_year.average_expense%type,
+   in in_revision_count farms.benchmark_year.revision_count%type,
+   in in_user farms.benchmark_year.update_user%type
 )
 language plpgsql
 as $$
@@ -14,7 +14,7 @@ begin
         average_expense = in_expense,
         update_timestamp = current_timestamp,
         update_user = in_user,
-        revision_count = (in_revision_count + 1),
+        revision_count = (in_revision_count + 1)
     where benchmark_per_unit_id = in_benchmark_per_unit_id
     and benchmark_year = in_year
     and revision_count = in_revision_count;
