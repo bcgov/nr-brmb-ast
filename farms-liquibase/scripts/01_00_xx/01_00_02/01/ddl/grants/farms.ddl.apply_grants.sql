984b3fa84c685fe0720c5b0c27c02e2262ff9cd3
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 15:38:53 -0700

    AS-3107: Create farms_codes_write_pkg.create_tip_line_item.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -98,6 +98,7 @@ GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_year_configuration_param
 GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.copy_year_configuration_params TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_tip_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_tip_line_item TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_tip_line_item TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
