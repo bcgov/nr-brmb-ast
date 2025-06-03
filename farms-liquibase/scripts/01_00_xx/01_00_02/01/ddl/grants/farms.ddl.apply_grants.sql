f547c6fba036f5890b470f27472d324d1e94d15d
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 12:01:42 -0700

    AS-3107: Create farms_codes_write_pkg.create_market_rate_premium.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -67,6 +67,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.create_inventory_xref TO "app_fa
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_inventory_xref TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_inventory_xref TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_inventory_xref TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_market_rate_premium TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
