775e311eb9753b98708256eafe445f56f51399bc
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 13:52:50 -0700

    AS-3107: Create farms_codes_write_pkg.delete_farm_type_3.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -78,6 +78,7 @@ GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_crop_unit_convrsn_factr 
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.recalculate_fmvs TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_farm_type_3 TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_farm_type_3 TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_farm_type_3 TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
