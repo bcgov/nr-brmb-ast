c6c2d8c3bc014ffc818e1a86ae3078fdecaeaf3e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 12:02:39 -0700

    AS-3107: Create farms_codes_write_pkg.create_market_rate_premium.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_codes_write_pkg.create_market_rate_premium.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_codes_write_pkg.create_market_rate_premium.sql
@@ -1,6 +1,6 @@
 create or replace procedure farms_codes_write_pkg.create_market_rate_premium(
-   in in_min_total_premium_amount farms.market_rate_premium.min_total_premium_amount%type,
-   in in_max_total_premium_amount farms.market_rate_premium.max_total_premium_amount%type,
+   in in_minimum_total_premium_amount farms.market_rate_premium.minimum_total_premium_amount%type,
+   in in_maximum_total_premium_amount farms.market_rate_premium.maximum_total_premium_amount%type,
    in in_risk_charge_flat_amount farms.market_rate_premium.risk_charge_flat_amount%type,
    in in_risk_charge_percent_premium farms.market_rate_premium.risk_charge_percent_premium%type,
    in in_adjust_charge_flat_amount farms.market_rate_premium.adjust_charge_flat_amount%type,
@@ -11,8 +11,8 @@ as $$
 begin
     insert into farms.market_rate_premium (
         market_rate_premium_id,
-        min_total_premium_amount,
-        max_total_premium_amount,
+        minimum_total_premium_amount,
+        maximum_total_premium_amount,
         risk_charge_flat_amount,
         risk_charge_percent_premium,
         adjust_charge_flat_amount,
@@ -23,8 +23,8 @@ begin
         update_date
     ) values (
         nextval('farms.seq_mrp'),
-        in_min_total_premium_amount,
-        in_max_total_premium_amount,
+        in_minimum_total_premium_amount,
+        in_maximum_total_premium_amount,
         in_risk_charge_flat_amount,
         in_risk_charge_percent_premium,
         in_adjust_charge_flat_amount,
