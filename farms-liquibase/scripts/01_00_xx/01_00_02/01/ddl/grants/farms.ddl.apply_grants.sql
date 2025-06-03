ea7d623a0ce2624c9c66f5297132267407763a70
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 09:30:00 -0700

    AS-3107: Create farms_codes_write_pkg.in_use_farm_type_code.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -47,6 +47,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_structure_group_code TO "
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_structure_group_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_farm_type_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_triage_queue_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_farm_type_code TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
