a5a9cbdc93266b102eab04b32ba4b9603998e940
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-03 09:52:15 -0700

    AS-3107: Create farms_codes_write_pkg.in_use_bpu.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -51,6 +51,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_farm_type_code TO "app_fa
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.delete_farm_type_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_farm_type_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_bpu_year TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_write_pkg.in_use_bpu TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
