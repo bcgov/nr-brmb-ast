d0dff71981a2de677c93b1333136eabd9e868dab
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-04 16:29:54 -0700

    AS-3107: Create farms_codes_read_pkg.read_inventory_xrefs.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -23,6 +23,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.export_bpu TO "app_farms_rest_pro
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.export_missing_bpu TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_inventory_item_codes TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_inventory_item_details TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_inventory_xrefs TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_sector_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_line_item TO "app_farms_rest_proxy";
