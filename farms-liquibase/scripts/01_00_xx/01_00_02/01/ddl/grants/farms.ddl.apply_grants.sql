1f5fcf7b4b812d86a816ab6df10c7b87e3923ab9
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-04 14:23:26 -0700

    AS-3107: Create farms_codes_read_pkg.read_federal_accounting_codes.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -10,6 +10,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_municipality_codes TO "app_f
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_municipality_office_codes TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_regional_office_codes TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_crop_unit_codes TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_federal_accounting_codes TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_sector_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_line_item TO "app_farms_rest_proxy";
