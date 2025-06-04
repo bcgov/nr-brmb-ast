2acb8bfd135c0da3e88c6224f5a5c527c0d7aa70
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-04 14:13:20 -0700

    AS-3107: Create farms_codes_read_pkg.read_municipality_codes.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -6,6 +6,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.check_line_item_exists_for_progra
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_fmv TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.export_fmv TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.export_missing_fmv TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_municipality_codes TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_sector_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_line_item TO "app_farms_rest_proxy";
