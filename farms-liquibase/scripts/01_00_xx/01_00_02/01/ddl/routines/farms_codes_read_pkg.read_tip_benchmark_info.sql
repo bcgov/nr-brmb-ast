91bd2366b8e58b3310918ec06de8ad7632ebe0e6
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-05 11:57:40 -0700

    AS-3107: Create farms_codes_read_pkg.read_tip_benchmark_info.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/routines/farms_codes_read_pkg.read_tip_benchmark_info.sql
@@ -0,0 +1,19 @@
+create or replace function farms_codes_read_pkg.read_tip_benchmark_info()
+returns refcursor
+language plpgsql
+as $$
+declare
+    cur refcursor;
+begin
+
+    open cur for
+        select trr.year,
+               count(*) operation_count,
+               max(trr.generated_date) generated_date
+        from farms.tip_report_result trr
+        where trr.parent_tip_report_result_id is null
+        group by trr.year
+        order by trr.year desc;
+    return cur;
+
+end;
