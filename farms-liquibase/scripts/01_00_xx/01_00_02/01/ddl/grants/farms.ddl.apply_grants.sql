d2a47a6d4651517851caf739d29721eb0ba6e642
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-05 09:56:46 -0700

    AS-3107: Create farms_codes_read_pkg.read_variance_limit_for_fruit_veg_code.

--- farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -38,6 +38,7 @@ GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_farm_type_default_income_ran
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_farm_type_2_income_range TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_farm_type_1_income_range TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_fruit_veg_codes TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_codes_read_pkg.read_variance_limit_for_fruit_veg_code TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_sector_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_line_item TO "app_farms_rest_proxy";
