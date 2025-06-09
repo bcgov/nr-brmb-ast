35ba9fd82dda33149477d0ac3ed192997746de63
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-09 11:15:11 -0700

    AS-3133: Add grant for farms.agri_scenarios_vw.

--- farms-liquibase/scripts/01_00_xx/01_00_02/02/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/02/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,5 @@
+GRANT SELECT ON farms.agri_scenarios_vw TO "app_farms_rest_proxy";
+
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_sector_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.create_line_item TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_codes_write_pkg.update_line_item TO "app_farms_rest_proxy";
