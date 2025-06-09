0e1d7615972b91016e0001a7fe6f70a5bed5d369
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-09 16:12:41 -0700

    AS-3135: Create farms_report_pkg.get_farm_type_detailed_codes.

--- farms-liquibase/scripts/01_00_xx/01_00_02/04/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/04/ddl/grants/farms.ddl.apply_grants.sql
@@ -3,3 +3,4 @@ GRANT EXECUTE ON FUNCTION farms_report_pkg.get_sector_detail_code TO "app_farms_
 GRANT EXECUTE ON FUNCTION farms_report_pkg.get_sector_and_detail TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_report_pkg.national_surveillance_strategy TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_report_pkg.analytical_surveillance_strategy TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_report_pkg.get_farm_type_detailed_codes TO "app_farms_rest_proxy";
