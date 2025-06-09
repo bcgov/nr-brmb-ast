9b5cfc97f63b915331cef5c56bcfa4334dab1a66
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-09 14:11:14 -0700

    AS-3135: Create farms_report_pkg.get_sector_and_detail.

--- farms-liquibase/scripts/01_00_xx/01_00_02/04/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/04/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,2 +1,3 @@
 GRANT EXECUTE ON FUNCTION farms_report_pkg.get_latest_status TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_report_pkg.get_sector_detail_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_report_pkg.get_sector_and_detail TO "app_farms_rest_proxy";
