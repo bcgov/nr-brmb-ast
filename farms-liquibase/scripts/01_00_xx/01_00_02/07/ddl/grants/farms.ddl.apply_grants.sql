31524fbb5f633ec6099306003e057323717f3200
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-10 14:05:17 -0700

    AS-3140: Create farms_version_pkg.import_failure.

--- farms-liquibase/scripts/01_00_xx/01_00_02/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_02/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -5,3 +5,4 @@ GRANT EXECUTE ON FUNCTION farms_version_pkg.uploaded_version TO "app_farms_rest_
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.upload_failure TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.start_import TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.perform_import TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_version_pkg.import_failure TO "app_farms_rest_proxy";
