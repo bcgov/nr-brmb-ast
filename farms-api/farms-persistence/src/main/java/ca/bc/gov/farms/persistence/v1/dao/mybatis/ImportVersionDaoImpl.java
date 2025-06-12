f310486deb880af62d6f14c87090269437740b65
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-12 10:08:30 -0700

    AS-3092: Use JDBC instead of MyBatis for uploadFailure.

--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/ImportVersionDaoImpl.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/ImportVersionDaoImpl.java
@@ -140,13 +140,13 @@ public class ImportVersionDaoImpl extends BaseDao implements ImportVersionDao {
     public void uploadFailure(Long versionId, String message, String user) throws DaoException {
         logger.debug("<uploadFailure");
 
-        try {
-            Map<String, Object> parameters = new HashMap<>();
-            parameters.put("versionId", versionId);
-            parameters.put("message", message);
-            parameters.put("user", user);
-            this.mapper.uploadFailure(parameters);
-        } catch (RuntimeException e) {
+        try (CallableStatement callableStatement = this.conn
+                .prepareCall("{ call farms_version_pkg.upload_failure(?, ?, ?) }")) {
+            callableStatement.setLong(1, versionId);
+            callableStatement.setString(2, message);
+            callableStatement.setString(3, user);
+            callableStatement.execute();
+        } catch (RuntimeException | SQLException e) {
             handleException(e);
         }
 
