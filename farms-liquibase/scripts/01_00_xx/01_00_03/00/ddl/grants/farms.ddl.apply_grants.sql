eca8e4c45b9c6917e8b483598fd37a01b55a67ab
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-13 16:22:41 -0700

    AS-3092: Implement ImportEndpointsImpl.

--- farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
@@ -12,3 +12,6 @@ GRANT USAGE ON SCHEMA farms_read_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_report_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_version_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_webapp_pkg TO "app_farms_rest_proxy";
+
+GRANT TRUNCATE ON TABLE farms.zbpu_benchmark_per_unit TO "app_farms_rest_proxy";
+GRANT TRUNCATE ON TABLE farms.import_log TO "app_farms_rest_proxy";
