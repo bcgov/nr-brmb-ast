58b66640e77cd59c28affa0917157af6966aed1e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-13 15:02:41 -0700

    AS-3092: Implement ImportEndpointsImpl.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/ImportEndpoints.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/ImportEndpoints.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,6 +18,7 @@ public interface ImportEndpoints {
     @POST
     @Path("/bpu")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     Response importBPU(@FormDataParam("file") InputStream fileInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetail) throws Exception;
 }
