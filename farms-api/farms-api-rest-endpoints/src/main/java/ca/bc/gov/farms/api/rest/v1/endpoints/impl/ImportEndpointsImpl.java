b9f90cf48672584310995912c35361ec1eef296b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-13 15:59:05 -0700

    AS-3092: Implement ImportEndpointsImpl.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/impl/ImportEndpointsImpl.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/impl/ImportEndpointsImpl.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.farms.api.rest.v1.endpoints.impl;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,11 +11,11 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import ca.bc.gov.farms.api.rest.v1.endpoints.ImportEndpoints;
 import ca.bc.gov.farms.domain.codes.ImportClassCodes;
 import ca.bc.gov.farms.domain.codes.ImportStateCodes;
 import ca.bc.gov.farms.persistence.v1.dto.ImportVersionDto;
+import ca.bc.gov.farms.service.api.v1.ImportBPUService;
 import ca.bc.gov.farms.service.api.v1.ImportService;
 
 public class ImportEndpointsImpl extends BaseEndpointsImpl implements ImportEndpoints {
@@ -23,6 +24,9 @@ public class ImportEndpointsImpl extends BaseEndpointsImpl implements ImportEndp
 
     @Autowired
     private ImportService importService;
+
+    @Autowired
+    private ImportBPUService importBPUService;
 
     @Override
     public Response importBPU(InputStream fileInputStream, FormDataContentDisposition fileDetail) throws Exception {
@@ -40,6 +44,10 @@ public class ImportEndpointsImpl extends BaseEndpointsImpl implements ImportEndp
             ImportVersionDto importVersionDto = importService.createImportVersion(
                     ImportClassCodes.BPU, ImportStateCodes.SCHEDULED_FOR_STAGING, ImportClassCodes.BPU_DESCRIPTION,
                     fileName, fileContent, "UserId");
+
+            Long importVersionId = importVersionDto.getImportVersionId();
+            InputStream inputStream = new ByteArrayInputStream(fileContent);
+            importBPUService.importCSV(importVersionId, inputStream, "UserId");
 
             response = Response.ok(importVersionDto).build();
         } catch (Exception e) {
--- farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
@@ -3,4 +3,12 @@ GRANT EXECUTE ON PROCEDURE farms_version_pkg.import_failure TO "app_farms_rest_p
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.import_complete TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_webapp_pkg.insert_import_version TO "app_farms_rest_proxy";
 
+GRANT USAGE ON SCHEMA farms_bpu_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_codes_read_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_codes_write_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_error_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_import_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_read_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_report_pkg TO "app_farms_rest_proxy";
+GRANT USAGE ON SCHEMA farms_version_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_webapp_pkg TO "app_farms_rest_proxy";
