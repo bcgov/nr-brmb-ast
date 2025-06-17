8a24e6c7d564f0f05579e10d21adf1825b94f3d9
Hong-Yi Wang <hwang@vividsolutions.com>
2025-06-17 09:49:50 -0700

    AS-3092: Fix farms_bpu_pkg.staging_to_operational.

--- farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/00/ddl/grants/farms.ddl.apply_grants.sql
@@ -2,6 +2,7 @@ GRANT EXECUTE ON PROCEDURE farms_version_pkg.uploaded_version TO "app_farms_rest
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.import_failure TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_version_pkg.import_complete TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_webapp_pkg.insert_import_version TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.staging_to_operational TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_bpu_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_codes_read_pkg TO "app_farms_rest_proxy";
