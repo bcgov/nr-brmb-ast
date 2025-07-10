e1790f1f4be49003e50ac3175f4ac279aa30fb1e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-10 12:58:22 -0700

    AS-3122: Create farms_staging_pkg.clear.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_03/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -0,0 +1,5 @@
+GRANT EXECUTE ON PROCEDURE farms_staging_pkg.clear TO "app_farms_rest_proxy";
+
+GRANT USAGE ON SCHEMA farms_staging_pkg TO "app_farms_rest_proxy";
+
+--GRANT TRUNCATE ON TABLE farms.zbpu_benchmark_per_unit TO "app_farms_rest_proxy";
