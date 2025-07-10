d2e01d945f539d910c1fbdd992b61fff8c123376
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-10 14:20:45 -0700

    AS-3122: Create farms_staging_pkg.insert_z01.

--- farms-liquibase/scripts/01_00_xx/01_00_03/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON PROCEDURE farms_staging_pkg.clear TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_staging_pkg.insert_z01 TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_staging_pkg TO "app_farms_rest_proxy";
 
