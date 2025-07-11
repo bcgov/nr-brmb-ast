bdf772ee2a416e1a72d44a1610e239b3b0b0855c
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-11 15:16:45 -0700

    AS-3092: Implement StagingDao.

--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/StagingDao.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/StagingDao.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 import java.sql.SQLException;
 
 import ca.bc.gov.farms.persistence.v1.dto.staging.Z01ParticipantInfo;
+import ca.bc.gov.farms.persistence.v1.dto.staging.Z02PartpntFarmInfo;
 
 public interface StagingDao extends Serializable {
 
     void insert(final Z01ParticipantInfo obj, final String userId) throws SQLException;
+
+    void insert(final Z02PartpntFarmInfo obj, final String userId) throws SQLException;
 }
--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/StagingDaoImpl.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/StagingDaoImpl.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import ca.bc.gov.brmb.common.persistence.dao.mybatis.BaseDao;
 import ca.bc.gov.farms.persistence.v1.dao.StagingDao;
 import ca.bc.gov.farms.persistence.v1.dto.staging.Z01ParticipantInfo;
+import ca.bc.gov.farms.persistence.v1.dto.staging.Z02PartpntFarmInfo;
 
 public class StagingDaoImpl extends BaseDao implements StagingDao {
 
@@ -18,11 +19,12 @@ public class StagingDaoImpl extends BaseDao implements StagingDao {
     }
 
     @Override
-    public void insert(Z01ParticipantInfo obj, String userId) throws SQLException {
+    public void insert(final Z01ParticipantInfo obj, final String userId) throws SQLException {
         int i = 1;
         try (CallableStatement callableStatement = this.conn
                 .prepareCall(
                         "call farms_staging_pkg.insert_z01(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+
             callableStatement.setInt(i++, obj.getParticipantPin());
             callableStatement.setString(i++, obj.getSinCtnBn());
             callableStatement.setString(i++, obj.getSin());
@@ -58,6 +60,68 @@ public class StagingDaoImpl extends BaseDao implements StagingDao {
             callableStatement.setInt(i++, obj.getPublicOffice());
             callableStatement.setString(i++, obj.getIdentEffectiveDate());
             callableStatement.setString(i++, userId);
+
+            callableStatement.execute();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public void insert(final Z02PartpntFarmInfo obj, final String userId) throws SQLException {
+        int i = 1;
+        try (CallableStatement callableStatement = this.conn
+                .prepareCall(
+                        "call farms_staging_pkg.insert_z02(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+
+            callableStatement.setInt(i++, obj.getParticipantPin());
+            callableStatement.setInt(i++, obj.getProgramYear());
+            callableStatement.setInt(i++, obj.getFormVersionNumber());
+
+            callableStatement.setString(i++, obj.getProvinceOfResidence());
+            callableStatement.setString(i++, obj.getProvinceOfMainFarmstead());
+            callableStatement.setString(i++, obj.getPostmarkDate());
+            callableStatement.setString(i++, obj.getReceivedDate());
+
+            callableStatement.setString(i++,
+                    obj.isSoleProprietor() == null ? null : obj.isSoleProprietor() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isPartnershipMember() == null ? null : obj.isPartnershipMember() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isCorporateShareholder() == null ? null : obj.isCorporateShareholder() ? "Y" : "N");
+            callableStatement.setString(i++, obj.isCoopMember() == null ? null : obj.isCoopMember() ? "Y" : "N");
+
+            callableStatement.setInt(i++, obj.getCommonShareTotal());
+            callableStatement.setInt(i++, obj.getFarmYears());
+            callableStatement.setString(i++,
+                    obj.isLastYearFarming() == null ? null : obj.isLastYearFarming() ? "Y" : "N");
+            callableStatement.setInt(i++, obj.getFormOriginCode());
+            callableStatement.setInt(i++, obj.getIndustryCode());
+            callableStatement.setInt(i++, obj.getParticipantProfileCode());
+
+            callableStatement.setString(i++,
+                    obj.isAccrualCashConversion() == null ? null : obj.isAccrualCashConversion() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isPerishableCommodities() == null ? null : obj.isPerishableCommodities() ? "Y" : "N");
+            callableStatement.setString(i++, obj.isReceipts() == null ? null : obj.isReceipts() ? "Y" : "N");
+            callableStatement.setString(i++, obj.isOtherText() == null ? null : obj.isOtherText() ? "Y" : "N");
+            callableStatement.setString(i++, obj.getOtherText());
+
+            callableStatement.setString(i++,
+                    obj.isAccrualWorksheet() == null ? null : obj.isAccrualWorksheet() ? "Y" : "N");
+            callableStatement.setString(i++, obj.isCwbWorksheet() == null ? null : obj.isCwbWorksheet() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isCombinedThisYear() == null ? null : obj.isCombinedThisYear() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isCompletedProdCycle() == null ? null : obj.isCompletedProdCycle() ? "Y" : "N");
+            callableStatement.setString(i++, obj.isDisaster() == null ? null : obj.isDisaster() ? "Y" : "N");
+            callableStatement.setString(i++,
+                    obj.isCopyCobToContact() == null ? null : obj.isCopyCobToContact() ? "Y" : "N");
+
+            callableStatement.setInt(i++, obj.getMunicipalityCode());
+            callableStatement.setString(i++, obj.getFormVersionEffectiveDate());
+            callableStatement.setString(i++, userId);
+
             callableStatement.execute();
         } catch (SQLException e) {
             throw e;
