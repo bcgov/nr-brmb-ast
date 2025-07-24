efe6457fd392c1b810c1be90b3e4616b359e1972
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-24 10:49:12 -0700

    AS-3195: Implement farms_fmv_pkg.validate_period.

--- farms-liquibase/scripts/01_00_xx/01_00_03/03/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/03/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,2 +1,3 @@
 GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.insert_error TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.validate_program_year TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.validate_period TO "app_farms_rest_proxy";
