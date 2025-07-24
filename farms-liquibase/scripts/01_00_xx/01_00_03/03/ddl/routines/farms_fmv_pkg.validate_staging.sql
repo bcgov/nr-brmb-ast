0a6df1069b64eb72e175457d80fa6a7c4c30c85a
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-24 13:07:12 -0700

    AS-3195: Implement farms_fmv_pkg.validate_staging.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_03/03/ddl/routines/farms_fmv_pkg.validate_staging.sql
@@ -0,0 +1,21 @@
+create or replace procedure farms_fmv_pkg.validate_staging(
+   in in_import_version_id numeric
+)
+language plpgsql
+as $$
+begin
+    delete from farms.import_log
+    where import_version_id = in_import_version_id;
+
+    call farms_fmv_pkg.validate_program_year(in_import_version_id);
+    call farms_fmv_pkg.validate_period(in_import_version_id);
+    call farms_fmv_pkg.validate_variance(in_import_version_id);
+    call farms_fmv_pkg.validate_municipality(in_import_version_id);
+    call farms_fmv_pkg.validate_unit(in_import_version_id);
+    call farms_fmv_pkg.validate_inventory(in_import_version_id);
+    call farms_fmv_pkg.validate_xref_exists(in_import_version_id);
+    call farms_fmv_pkg.validate_uniqueness(in_import_version_id);
+    call farms_fmv_pkg.validate_one_unit_per_inventry(in_import_version_id);
+    call farms_fmv_pkg.validate_defaults(in_import_version_id);
+end;
+$$;
