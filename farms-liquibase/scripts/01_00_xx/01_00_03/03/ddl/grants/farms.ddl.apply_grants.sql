233d47c22d7bb84102bf39cb2cc1ca44c1d55835
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-24 13:12:08 -0700

    AS-3195: Implement farms_fmv_pkg.delete_staging_errors.

--- farms-liquibase/scripts/01_00_xx/01_00_03/03/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/03/ddl/grants/farms.ddl.apply_grants.sql
@@ -13,3 +13,4 @@ GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.clear_staging TO "app_farms_rest_proxy"
 GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.insert_staging_row TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.validate_staging TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_fmv_pkg.get_staging_errors TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_fmv_pkg.delete_staging_errors TO "app_farms_rest_proxy";
