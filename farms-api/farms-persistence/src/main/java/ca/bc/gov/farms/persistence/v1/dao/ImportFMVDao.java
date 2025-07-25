4d853002fd5a45ce9c23dc4f9949cb46c5f253a5
Hong-Yi Wang <hwang@vividsolutions.com>
2025-07-25 09:50:19 -0700

    AS-3195: Implement insertStagingRow.

--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/ImportFMVDao.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/ImportFMVDao.java
@@ -3,8 +3,11 @@ package ca.bc.gov.farms.persistence.v1.dao;
 import java.io.Serializable;
 
 import ca.bc.gov.brmb.common.persistence.dao.DaoException;
+import ca.bc.gov.farms.persistence.v1.dto.ImportFMVDto;
 
 public interface ImportFMVDao extends Serializable {
 
     void clearStaging() throws DaoException;
+
+    void insertStagingRow(ImportFMVDto dto, String userId, int rowNum) throws DaoException;
 }
--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/ImportFMVDaoImpl.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/ImportFMVDaoImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import ca.bc.gov.brmb.common.persistence.dao.DaoException;
 import ca.bc.gov.brmb.common.persistence.dao.mybatis.BaseDao;
 import ca.bc.gov.farms.persistence.v1.dao.ImportFMVDao;
+import ca.bc.gov.farms.persistence.v1.dto.ImportFMVDto;
 
 public class ImportFMVDaoImpl extends BaseDao implements ImportFMVDao {
 
@@ -35,5 +36,29 @@ public class ImportFMVDaoImpl extends BaseDao implements ImportFMVDao {
         }
 
         logger.debug(">clearStaging");
+    }
+
+    @Override
+    public void insertStagingRow(ImportFMVDto dto, String userId, int rowNum) throws DaoException {
+        logger.debug("<insertStagingRow");
+
+        try (CallableStatement callableStatement = this.conn
+                .prepareCall(
+                        "call farms_fmv_pkg.insert_staging_row(?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            callableStatement.setInt(1, rowNum);
+            callableStatement.setInt(2, dto.getProgramYear());
+            callableStatement.setInt(3, dto.getPeriod());
+            callableStatement.setBigDecimal(4, dto.getAveragePrice());
+            callableStatement.setBigDecimal(5, dto.getPercentVariance());
+            callableStatement.setString(6, dto.getMunicipalityCode());
+            callableStatement.setString(7, dto.getCropUnitCode());
+            callableStatement.setString(8, dto.getInventoryItemCode());
+            callableStatement.setString(9, userId);
+            callableStatement.execute();
+        } catch (RuntimeException | SQLException e) {
+            handleException(e);
+        }
+
+        logger.debug(">insertStagingRow");
     }
 }
