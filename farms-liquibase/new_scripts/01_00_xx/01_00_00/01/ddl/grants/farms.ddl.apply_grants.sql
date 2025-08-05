6dd7ad52f2e6ea557770a8c25be6d0c526308914
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-05 10:04:57 -0700

    AS-3209: Implement function farms_bpu_pkg.margin_data_differences.

--- farms-liquibase/new_scripts/01_00_xx/01_00_00/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/new_scripts/01_00_xx/01_00_00/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -8,3 +8,4 @@ GRANT EXECUTE ON FUNCTION farms_bpu_pkg.get_staging_errors TO "app_farms_rest_pr
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.insert_error TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.insert_staging_row TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_bpu_pkg.is_group_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_bpu_pkg.margin_data_differences TO "app_farms_rest_proxy";
