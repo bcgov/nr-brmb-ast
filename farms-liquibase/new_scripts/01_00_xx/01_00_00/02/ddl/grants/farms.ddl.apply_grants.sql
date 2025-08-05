7560030f045e32764e8dcf2010bc107b44b21fe7
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-05 11:45:27 -0700

    AS-3209: Implement function farms_error_pkg.codify_mun_code.

--- farms-liquibase/new_scripts/01_00_xx/01_00_00/02/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/new_scripts/01_00_xx/01_00_00/02/ddl/grants/farms.ddl.apply_grants.sql
@@ -4,3 +4,4 @@ GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_benefit_margins TO "app_farms_r
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_claim TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_income_expense TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_inventory_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_mun_code TO "app_farms_rest_proxy";
