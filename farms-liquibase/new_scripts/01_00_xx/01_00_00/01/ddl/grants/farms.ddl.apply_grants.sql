80d7c77b1fd2c0a63a480010961bbcf81b1382df
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-05 09:49:30 -0700

    AS-3209: Implement function farms_bpu_pkg.get_staging_errors.

--- farms-liquibase/new_scripts/01_00_xx/01_00_00/01/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/new_scripts/01_00_xx/01_00_00/01/ddl/grants/farms.ddl.apply_grants.sql
@@ -4,3 +4,4 @@ GRANT TRUNCATE ON TABLE farms.farm_import_logs TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.clear_staging TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.delete_staging_errors TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_bpu_pkg.expense_data_differences TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_bpu_pkg.get_staging_errors TO "app_farms_rest_proxy";
