62509c323ac3e6ddc5a39e1e539ca4a8fcf8cc5a
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-05 11:42:08 -0700

    AS-3209: Implement function farms_error_pkg.codify_income_expense.

--- farms-liquibase/new_scripts/01_00_xx/01_00_00/00/ddl/references/farms.farm_reported_income_expenses.sql
+++ farms-liquibase/new_scripts/01_00_xx/01_00_00/00/ddl/references/farms.farm_reported_income_expenses.sql
@@ -1,1 +1,5 @@
+ALTER TABLE farms.farm_reported_income_expenses ADD CONSTRAINT farm_rie_farm_as_fk FOREIGN KEY (agristability_scenario_id) REFERENCES farms.farm_agristability_scenarios(agristability_scenario_id) ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE farms.farm_reported_income_expenses ADD CONSTRAINT farm_rie_farm_fo_fk FOREIGN KEY (farming_operation_id) REFERENCES farms.farm_farming_operations(farming_operation_id) ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE;
+
 ALTER TABLE farms.farm_reported_income_expenses ADD CONSTRAINT farm_rie_farm_rie_fk FOREIGN KEY (cra_reported_income_expense_id) REFERENCES farms.farm_reported_income_expenses(reported_income_expense_id) ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE;
--- farms-liquibase/new_scripts/01_00_xx/01_00_00/02/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/new_scripts/01_00_xx/01_00_00/02/ddl/grants/farms.ddl.apply_grants.sql
@@ -2,3 +2,4 @@ GRANT USAGE ON SCHEMA farms_error_pkg TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_benefit_margins TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_claim TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_error_pkg.codify_income_expense TO "app_farms_rest_proxy";
