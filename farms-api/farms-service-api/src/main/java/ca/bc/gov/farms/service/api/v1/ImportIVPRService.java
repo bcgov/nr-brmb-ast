790f81cce5d42c9f4bbabb61cba12ef730cbb761
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-18 13:16:21 -0700

    AS-3110: Implement ImportIVPRService.

---
+++ farms-api/farms-service-api/src/main/java/ca/bc/gov/farms/service/api/v1/ImportIVPRService.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.farms.service.api.v1;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface ImportIVPRService {
+
+    List<Object> getStagingErrors();
+
+    void importCSV(Long importVersionId, InputStream inputStream, String userId);
+
+    void processImport(Long importVersionId, String userId);
+}
