2a453347dc317f8a1035c53c3c91662e51371783
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-18 11:27:00 -0700

    AS-3110: Create procedure farms_ivpr_pkg.insert_staging_row.

--- farms-liquibase/scripts/01_00_xx/01_00_03/04/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/04/ddl/grants/farms.ddl.apply_grants.sql
@@ -3,3 +3,4 @@ GRANT TRUNCATE ON TABLE farms.farm_zivpr_iv_premium_rates TO "app_farms_rest_pro
 
 GRANT EXECUTE ON PROCEDURE farms_ivpr_pkg.insert_error TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_ivpr_pkg.validate_inventory TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_ivpr_pkg.insert_staging_row TO "app_farms_rest_proxy";
