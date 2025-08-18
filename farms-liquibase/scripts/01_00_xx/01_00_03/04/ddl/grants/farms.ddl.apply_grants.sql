e6bac16fb0368261d5050f34ef36b3db13163f91
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-18 11:22:12 -0700

    AS-3110: Create procedure farms_ivpr_pkg.validate_inventory.

--- farms-liquibase/scripts/01_00_xx/01_00_03/04/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/04/ddl/grants/farms.ddl.apply_grants.sql
@@ -2,3 +2,4 @@ GRANT USAGE ON SCHEMA farms_ivpr_pkg TO "app_farms_rest_proxy";
 GRANT TRUNCATE ON TABLE farms.farm_zivpr_iv_premium_rates TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON PROCEDURE farms_ivpr_pkg.insert_error TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_ivpr_pkg.validate_inventory TO "app_farms_rest_proxy";
