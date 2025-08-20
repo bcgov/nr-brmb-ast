cd17995f91295f5bb40e510fc02a5bbf8d8c2683
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-20 12:07:16 -0700

    AS-3207: Create farmsDataSource that connects to testcontainers.

---
+++ farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/api/rest/v1/spring/EndpointsSpringConfigTest.java
@@ -0,0 +1,32 @@
+package ca.bc.gov.farms.api.rest.v1.spring;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Configuration
+public class EndpointsSpringConfigTest {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:17-alpine")
+                .withDatabaseName("farmsdev")
+                .withUsername("app_farms")
+                .withPassword("test1234");
+    }
+
+    @Primary
+    @Bean
+    public DataSource farmsDataSource(PostgreSQLContainer<?> postgresContainer) {
+        return DataSourceBuilder.create()
+                .url(postgresContainer.getJdbcUrl())
+                .username(postgresContainer.getUsername())
+                .password(postgresContainer.getPassword())
+                .driverClassName(postgresContainer.getDriverClassName())
+                .build();
+    }
+}
--- farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/persistence/v1/dao/BenchmarkPerUnitDaoTest.java
+++ farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/persistence/v1/dao/BenchmarkPerUnitDaoTest.java
@@ -1,17 +1,31 @@
 package ca.bc.gov.farms.persistence.v1.dao;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
-
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ca.bc.gov.farms.api.rest.v1.spring.EndpointsSpringConfigTest;
+import ca.bc.gov.farms.persistence.v1.dto.BenchmarkPerUnitDto;
 import ca.bc.gov.farms.persistence.v1.spring.PersistenceSpringConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
-@ContextConfiguration(classes = { PersistenceSpringConfig.class })
+@ExtendWith(SpringExtension.class)
+@ComponentScan(basePackages = "ca.bc.gov.farms")
+@ContextConfiguration(classes = { EndpointsSpringConfigTest.class, PersistenceSpringConfig.class })
 public class BenchmarkPerUnitDaoTest {
+
+    @Autowired
+    private BenchmarkPerUnitDao benchmarkPerUnitDao;
 
     @Test
     public void testDemo() {
-        assertThat(Boolean.TRUE).isFalse();
+        assertThatNoException().isThrownBy(() -> {
+            BenchmarkPerUnitDto dto = benchmarkPerUnitDao.fetch(1L);
+            assertThat(dto).isNull();
+        });
     }
 }
