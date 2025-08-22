08c2ebd74aad3ed12e60dd6ba16b6821a164b104
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-22 16:13:10 -0700

    AS-3207: Implement InventoryItemDetailDaoTest.testFetchByInventoryItemCode.

--- farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/persistence/v1/dao/InventoryItemDetailDaoTest.java
+++ farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/persistence/v1/dao/InventoryItemDetailDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -68,5 +69,35 @@ public class InventoryItemDetailDaoTest {
         assertThat(result.getFruitVegTypeDesc()).isNull();
         assertThat(result.getMultiStageCommdtyCode()).isNull();
         assertThat(result.getMultiStageCommdtyDesc()).isNull();
+    }
+
+    @Test
+    @Order(2)
+    public void testFetchByInventoryItemCode() {
+        List<InventoryItemDetailDto> dtos = null;
+        try {
+            dtos = inventoryItemDetailDao.fetchByInventoryItemCode("7208");
+        } catch (DaoException e) {
+            fail(e.getMessage());
+            return;
+        }
+        assertThat(dtos).isNotNull();
+        assertThat(dtos).isNotEmpty();
+        assertThat(dtos.size()).isEqualTo(1);
+
+        InventoryItemDetailDto fetchedDto = dtos.get(0);
+        assertThat(fetchedDto.getProgramYear()).isEqualTo(2024);
+        assertThat(fetchedDto.getEligibilityInd()).isEqualTo("N");
+        assertThat(fetchedDto.getLineItem()).isNull();
+        assertThat(fetchedDto.getInsurableValue()).isEqualTo(new BigDecimal("24000.000"));
+        assertThat(fetchedDto.getPremiumRate()).isEqualTo(new BigDecimal("0.0464"));
+        assertThat(fetchedDto.getInventoryItemCode()).isEqualTo("7208");
+        assertThat(fetchedDto.getInventoryItemDesc()).isEqualTo("Daffodils; Fresh cut");
+        assertThat(fetchedDto.getCommodityTypeCode()).isNull();
+        assertThat(fetchedDto.getCommodityTypeDesc()).isNull();
+        assertThat(fetchedDto.getFruitVegTypeCode()).isNull();
+        assertThat(fetchedDto.getFruitVegTypeDesc()).isNull();
+        assertThat(fetchedDto.getMultiStageCommdtyCode()).isNull();
+        assertThat(fetchedDto.getMultiStageCommdtyDesc()).isNull();
     }
 }
