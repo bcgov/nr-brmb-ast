11e69a37a66ea3f66b4243ed03651e95b34bec4e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-26 09:47:30 -0700

    AS-3247: Implement InventoryTypeXrefListRsrc.

---
+++ farms-api/farms-api-rest-common/src/main/java/ca/bc/gov/farms/api/rest/v1/resource/InventoryTypeXrefListRsrc.java
@@ -0,0 +1,38 @@
+package ca.bc.gov.farms.api.rest.v1.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import ca.bc.gov.brmb.common.rest.resource.BaseResource;
+import ca.bc.gov.farms.api.rest.v1.resource.types.ResourceTypes;
+import ca.bc.gov.farms.model.v1.InventoryTypeXrefList;
+
+@XmlRootElement(namespace = ResourceTypes.NAMESPACE, name = ResourceTypes.INVENTORY_ITEM_DETAIL_LIST_NAME)
+@XmlSeeAlso({ InventoryTypeXrefListRsrc.class })
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+public class InventoryTypeXrefListRsrc extends BaseResource implements InventoryTypeXrefList<InventoryTypeXrefRsrc> {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<InventoryTypeXrefRsrc> inventoryTypeXrefList;
+
+    public InventoryTypeXrefListRsrc() {
+        this.inventoryTypeXrefList = new ArrayList<>();
+    }
+
+    @Override
+    public List<InventoryTypeXrefRsrc> getInventoryTypeXrefList() {
+        return inventoryTypeXrefList;
+    }
+
+    @Override
+    public void setInventoryTypeXrefList(List<InventoryTypeXrefRsrc> inventoryTypeXrefList) {
+        this.inventoryTypeXrefList = inventoryTypeXrefList;
+    }
+
+}
