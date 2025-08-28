2fd9a195b9c1b2bcf18bf12b12cdd7ef526b3397
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-28 10:30:38 -0700

    AS-3248: Implement InventoryItemAttributeService.

---
+++ farms-api/farms-service-api/src/main/java/ca/bc/gov/farms/service/api/v1/InventoryItemAttributeService.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.farms.service.api.v1;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.bc.gov.brmb.common.service.api.NotFoundException;
+import ca.bc.gov.brmb.common.service.api.ServiceException;
+import ca.bc.gov.brmb.common.service.api.ValidationFailureException;
+import ca.bc.gov.brmb.common.service.api.model.factory.FactoryContext;
+import ca.bc.gov.farms.model.v1.InventoryItemAttribute;
+import ca.bc.gov.farms.model.v1.InventoryItemAttributeList;
+
+public interface InventoryItemAttributeService {
+
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    InventoryItemAttributeList<? extends InventoryItemAttribute> getInventoryItemAttributesByRollupInventoryItemCode(
+            String rollupInventoryItemCode, FactoryContext factoryContext) throws ServiceException;
+
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    InventoryItemAttribute getInventoryItemAttribute(Long inventoryItemAttributeId, FactoryContext factoryContext)
+            throws ServiceException, NotFoundException;
+
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    InventoryItemAttribute createInventoryItemAttribute(InventoryItemAttribute InventoryItemAttribute,
+            FactoryContext factoryContext)
+            throws ServiceException, ValidationFailureException;
+
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    InventoryItemAttribute updateInventoryItemAttribute(Long inventoryItemAttributeId,
+            InventoryItemAttribute InventoryItemAttribute, FactoryContext factoryContext)
+            throws ServiceException, ValidationFailureException, NotFoundException;
+
+    @Transactional(readOnly = false, rollbackFor = Exception.class)
+    void deleteInventoryItemAttribute(Long inventoryItemAttributeId)
+            throws ServiceException, NotFoundException;
+}
