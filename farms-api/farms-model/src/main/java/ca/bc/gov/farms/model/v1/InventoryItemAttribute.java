cd412cb852151ce0946dbb049ab967a0ed11d1c3
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-28 09:45:56 -0700

    AS-3248: Implement InventoryItemAttribute.

---
+++ farms-api/farms-model/src/main/java/ca/bc/gov/farms/model/v1/InventoryItemAttribute.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.farms.model.v1;
+
+import java.io.Serializable;
+
+public interface InventoryItemAttribute extends Serializable {
+
+    public Long getInventoryItemAttributeId();
+    public void setInventoryItemAttributeId(Long inventoryItemAttributeId);
+
+    public String getInventoryItemCode();
+    public void setInventoryItemCode(String inventoryItemCode);
+
+    public String getInventoryItemDesc();
+    public void setInventoryItemDesc(String inventoryItemDesc);
+
+    public String getRollupInventoryItemCode();
+    public void setRollupInventoryItemCode(String rollupInventoryItemCode);
+
+    public String getRollupInventoryItemDesc();
+    public void setRollupInventoryItemDesc(String rollupInventoryItemDesc);
+}
