75941e14b6699dd27c04171ef623ab98b98e0751
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-28 12:11:16 -0700

    AS-3248: Implement InventoryItemAttributeDaoTest.

---
+++ farms-api/farms-persistence/src/test/java/ca/bc/gov/farms/persistence/v1/dao/InventoryItemAttributeDaoTest.java
@@ -0,0 +1,116 @@
+package ca.bc.gov.farms.persistence.v1.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.bc.gov.brmb.common.persistence.dao.DaoException;
+import ca.bc.gov.farms.api.rest.v1.spring.EndpointsSpringConfigTest;
+import ca.bc.gov.farms.persistence.v1.dto.InventoryItemAttributeDto;
+import ca.bc.gov.farms.persistence.v1.spring.PersistenceSpringConfig;
+
+@ExtendWith(SpringExtension.class)
+@ComponentScan(basePackages = "ca.bc.gov.farms")
+@ContextConfiguration(classes = { EndpointsSpringConfigTest.class, PersistenceSpringConfig.class })
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class InventoryItemAttributeDaoTest {
+
+    @Autowired
+    private InventoryItemAttributeDao inventoryItemAttributeDao;
+
+    private static Long inventoryItemAttributeId;
+
+    @Test
+    @Order(1)
+    public void testInsert() {
+        InventoryItemAttributeDto dto = new InventoryItemAttributeDto();
+        dto.setInventoryItemCode("73");
+        dto.setRollupInventoryItemCode("73");
+
+        InventoryItemAttributeDto result = null;
+        try {
+            inventoryItemAttributeDao.insert(dto, "testUser");
+            inventoryItemAttributeId = dto.getInventoryItemAttributeId();
+            result = inventoryItemAttributeDao.fetch(inventoryItemAttributeId);
+        } catch (DaoException e) {
+            fail(e.getMessage());
+            return;
+        }
+
+        assertThat(result.getInventoryItemCode()).isEqualTo("73");
+        assertThat(result.getInventoryItemDesc()).isEqualTo("Strawberries");
+        assertThat(result.getRollupInventoryItemCode()).isEqualTo("73");
+        assertThat(result.getRollupInventoryItemDesc()).isEqualTo("Strawberries");
+    }
+
+    @Test
+    @Order(2)
+    public void testFetchByRollupInventoryItemCode() {
+        List<InventoryItemAttributeDto> dtos = null;
+        try {
+            dtos = inventoryItemAttributeDao.fetchByRollupInventoryItemCode("73");
+        } catch (DaoException e) {
+            fail(e.getMessage());
+            return;
+        }
+        assertThat(dtos).isNotNull();
+        assertThat(dtos).isNotEmpty();
+        assertThat(dtos.size()).isEqualTo(1);
+
+        InventoryItemAttributeDto fetchedDto = dtos.get(0);
+        assertThat(fetchedDto.getInventoryItemCode()).isEqualTo("73");
+        assertThat(fetchedDto.getInventoryItemDesc()).isEqualTo("Strawberries");
+        assertThat(fetchedDto.getRollupInventoryItemCode()).isEqualTo("73");
+        assertThat(fetchedDto.getRollupInventoryItemDesc()).isEqualTo("Strawberries");
+    }
+
+    @Test
+    @Order(3)
+    public void testUpdate() {
+        InventoryItemAttributeDto dto = null;
+        try {
+            dto = inventoryItemAttributeDao.fetch(inventoryItemAttributeId);
+        } catch (DaoException e) {
+            fail(e.getMessage());
+            return;
+        }
+        assertThat(dto).isNotNull();
+
+        dto.setInventoryItemCode("73");
+        dto.setRollupInventoryItemCode("5560");
+
+        InventoryItemAttributeDto result = null;
+        try {
+            inventoryItemAttributeDao.update(dto, "testUser");
+            result = inventoryItemAttributeDao.fetch(inventoryItemAttributeId);
+        } catch (DaoException e) {
+            fail(e.getMessage());
+            return;
+        }
+
+        assertThat(result.getInventoryItemCode()).isEqualTo("73");
+        assertThat(result.getInventoryItemDesc()).isEqualTo("Strawberries");
+        assertThat(result.getRollupInventoryItemCode()).isEqualTo("5560");
+        assertThat(result.getRollupInventoryItemDesc()).isEqualTo("Alfalfa Dehy");
+    }
+
+    @Test
+    @Order(4)
+    public void testDelete() {
+        assertThatNoException().isThrownBy(() -> {
+            inventoryItemAttributeDao.delete(inventoryItemAttributeId);
+        });
+    }
+}
