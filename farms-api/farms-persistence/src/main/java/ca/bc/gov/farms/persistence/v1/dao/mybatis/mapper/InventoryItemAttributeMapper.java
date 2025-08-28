6cdf1d667f4f5991289490bb6d73461a253c2197
Hong-Yi Wang <hwang@vividsolutions.com>
2025-08-28 09:58:20 -0700

    AS-3248: Implement InventoryItemAttributeMapper.

---
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/mapper/InventoryItemAttributeMapper.java
@@ -0,0 +1,19 @@
+package ca.bc.gov.farms.persistence.v1.dao.mybatis.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import ca.bc.gov.farms.persistence.v1.dto.InventoryItemAttributeDto;
+
+public interface InventoryItemAttributeMapper {
+
+    InventoryItemAttributeDto fetch(Map<String, Object> parameters);
+
+    List<InventoryItemAttributeDto> fetchBy(Map<String, Object> parameters);
+
+    int insertInventoryItemAttribute(Map<String, Object> parameters);
+
+    int updateInventoryItemAttribute(Map<String, Object> parameters);
+
+    int deleteInventoryItemAttribute(Map<String, Object> parameters);
+}
