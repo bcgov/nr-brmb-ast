b3787a216480fb8a1ea22a03164ddf049498e07a
Hong-Yi Wang <hwang@vividsolutions.com>
2025-09-18 11:01:29 -0700

    AS-3270: Implement LineItemEndpoints.

--- farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/LineItemEndpoints.java
+++ farms-api/farms-api-rest-endpoints/src/main/java/ca/bc/gov/farms/api/rest/v1/endpoints/LineItemEndpoints.java
@@ -13,8 +13,8 @@ import javax.ws.rs.core.Response;
 
 import ca.bc.gov.brmb.common.rest.resource.HeaderConstants;
 import ca.bc.gov.brmb.common.rest.resource.MessageListRsrc;
-import ca.bc.gov.farms.api.rest.v1.resource.ConfigurationParameterListRsrc;
-import ca.bc.gov.farms.api.rest.v1.resource.ConfigurationParameterRsrc;
+import ca.bc.gov.farms.api.rest.v1.resource.LineItemListRsrc;
+import ca.bc.gov.farms.api.rest.v1.resource.LineItemRsrc;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -27,11 +27,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@Path("/configurationParameters")
-@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-public interface ConfigurationParameterEndpoints {
+public interface LineItemEndpoints {
 
-    @Operation(operationId = "Get all Configuration Parameter resource.", summary = "Get all Configuration Parameter resource.", extensions = {
+    @Operation(operationId = "Get Line Item resource by Program Year.", summary = "Get Line Item resource by Program Year.", extensions = {
             @Extension(properties = {
                     @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
                     @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
@@ -43,13 +41,14 @@ public interface ConfigurationParameterEndpoints {
             @Parameter(name = HeaderConstants.AUTHORIZATION_HEADER, description = HeaderConstants.AUTHORIZATION_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER)
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ConfigurationParameterListRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = LineItemListRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))) })
     @GET
+    @Path("/lineItems")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response getAllConfigurationParameters();
+    public Response getLineItemsByProgramYear(Integer programYear);
 
-    @Operation(operationId = "Get Configuration Parameter resource by Configuration Parameter Id.", summary = "Get Configuration Parameter resource by Configuration Parameter Id.", extensions = {
+    @Operation(operationId = "Get Line Item resource by Line Item Id.", summary = "Get Line Item resource by Line Item Id.", extensions = {
             @Extension(properties = {
                     @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
                     @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
@@ -61,16 +60,16 @@ public interface ConfigurationParameterEndpoints {
             @Parameter(name = HeaderConstants.AUTHORIZATION_HEADER, description = HeaderConstants.AUTHORIZATION_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER)
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ConfigurationParameterRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = LineItemRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
             @ApiResponse(responseCode = "404", description = "Not Found"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))) })
     @GET
-    @Path("/{configurationParameterId}")
+    @Path("/lineItems/{lineItemId}")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response getConfigurationParameter(
-            @Parameter(description = "The identifier of the Configuration Parameter resource.") @PathParam("configurationParameterId") Long configurationParameterId);
+    public Response getLineItem(
+            @Parameter(description = "The identifier of the Line Item resource.") @PathParam("lineItemId") Long lineItemId);
 
-    @Operation(operationId = "Create Configuration Parameter resource.", summary = "Create Configuration Parameter resource.", extensions = {
+    @Operation(operationId = "Create Line Item resource.", summary = "Create Line Item resource.", extensions = {
             @Extension(properties = {
                     @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
                     @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
@@ -83,7 +82,7 @@ public interface ConfigurationParameterEndpoints {
             @Parameter(name = HeaderConstants.IF_MATCH_HEADER, description = HeaderConstants.IF_MATCH_DESCRIPTION, required = true, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER)
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(implementation = ConfigurationParameterRsrc.class)), headers = {
+            @ApiResponse(responseCode = "201", description = "Created", content = @Content(schema = @Schema(implementation = LineItemRsrc.class)), headers = {
                     @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION),
                     @Header(name = HeaderConstants.LOCATION_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.LOCATION_DESCRIPTION) }),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))),
@@ -92,12 +91,13 @@ public interface ConfigurationParameterEndpoints {
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class)))
     })
     @POST
+    @Path("/lineItems")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response createConfigurationParameter(
-            @Parameter(name = "configurationParameterRsrc", description = "The Configuration Parameter resource containing the new values.", required = true) ConfigurationParameterRsrc configurationParameterRsrc);
+    public Response createLineItem(
+            @Parameter(name = "lineItemRsrc", description = "The Line Item resource containing the new values.", required = true) LineItemRsrc lineItemRsrc);
 
-    @Operation(operationId = "Delete Configuration Parameter resource.", summary = "Delete Configuration Parameter resource.", extensions = {
+    @Operation(operationId = "Delete Line Item resource.", summary = "Delete Line Item resource.", extensions = {
             @Extension(properties = {
                     @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
                     @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
@@ -113,11 +113,11 @@ public interface ConfigurationParameterEndpoints {
             @ApiResponse(responseCode = "404", description = "Not Found"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))) })
     @DELETE
-    @Path("/{configurationParameterId}")
-    public Response deleteConfigurationParameter(
-            @Parameter(description = "The identifier of the Configuration Parameter resource.") @PathParam("configurationParameterId") Long configurationParameterId);
+    @Path("/lineItems/{lineItemId}")
+    public Response deleteLineItem(
+            @Parameter(description = "The identifier of the Line Item resource.") @PathParam("lineItemId") Long lineItemId);
 
-    @Operation(operationId = "Update Configuration Parameter resource.", summary = "Update Configuration Parameter resource.", extensions = {
+    @Operation(operationId = "Update Line Item resource.", summary = "Update Line Item resource.", extensions = {
             @Extension(properties = {
                     @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
                     @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
@@ -130,16 +130,39 @@ public interface ConfigurationParameterEndpoints {
             @Parameter(name = HeaderConstants.IF_MATCH_HEADER, description = HeaderConstants.IF_MATCH_DESCRIPTION, required = true, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER)
     })
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ConfigurationParameterRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = LineItemRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
             @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))),
             @ApiResponse(responseCode = "404", description = "Not Found"),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))) })
     @PUT
-    @Path("/{configurationParameterId}")
+    @Path("/lineItems/{lineItemId}")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response updateConfigurationParameter(
-            @Parameter(description = "The identifier of the Configuration Parameter resource.") @PathParam("configurationParameterId") Long configurationParameterId,
-            @Parameter(name = "configurationParameterRsrc", description = "The Configuration Parameter resource containing the new values.", required = true) ConfigurationParameterRsrc configurationParameterRsrc);
+    public Response updateLineItem(
+            @Parameter(description = "The identifier of the Line Item resource.") @PathParam("lineItemId") Long lineItemId,
+            @Parameter(name = "lineItemRsrc", description = "The Line Item resource containing the new values.", required = true) LineItemRsrc lineItemRsrc);
+
+    @Operation(operationId = "Copy Line Item resources.", summary = "Copy Line Item resources.", extensions = {
+            @Extension(properties = {
+                    @ExtensionProperty(name = "auth-type", value = "#{wso2.x-auth-type.app_and_app_user}"),
+                    @ExtensionProperty(name = "throttling-tier", value = "Unlimited") }) })
+    @Parameters({
+            @Parameter(name = HeaderConstants.REQUEST_ID_HEADER, description = HeaderConstants.REQUEST_ID_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER),
+            @Parameter(name = HeaderConstants.VERSION_HEADER, description = HeaderConstants.VERSION_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = Integer.class), in = ParameterIn.HEADER),
+            @Parameter(name = HeaderConstants.CACHE_CONTROL_HEADER, description = HeaderConstants.CACHE_CONTROL_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER),
+            @Parameter(name = HeaderConstants.PRAGMA_HEADER, description = HeaderConstants.PRAGMA_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER),
+            @Parameter(name = HeaderConstants.AUTHORIZATION_HEADER, description = HeaderConstants.AUTHORIZATION_HEADER_DESCRIPTION, required = false, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER),
+            @Parameter(name = HeaderConstants.IF_MATCH_HEADER, description = HeaderConstants.IF_MATCH_DESCRIPTION, required = true, schema = @Schema(implementation = String.class), in = ParameterIn.HEADER)
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = LineItemListRsrc.class)), headers = @Header(name = HeaderConstants.ETAG_HEADER, schema = @Schema(implementation = String.class), description = HeaderConstants.ETAG_DESCRIPTION)),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = MessageListRsrc.class))) })
+    @PUT
+    @Path("/copyLineItems/{currentYear}")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response copyLineItem(
+            @Parameter(description = "The current year.") @PathParam("currentYear") Integer currentYear);
 
 }
