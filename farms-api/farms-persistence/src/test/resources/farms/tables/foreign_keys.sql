de82c5a2784df1c0c9c98c15c0ec3fbaaf64d284
Hong-Yi Wang <hwang@vividsolutions.com>
2025-09-18 13:40:11 -0700

    AS-3270: Implement LineItemDaoTest.

--- farms-api/farms-persistence/src/test/resources/farms/tables/foreign_keys.sql
+++ farms-api/farms-persistence/src/test/resources/farms/tables/foreign_keys.sql
@@ -28,6 +28,9 @@ ALTER TABLE farms.farm_inventory_item_details ADD CONSTRAINT farm_iid_farm_fvtc_
 ALTER TABLE farms.farm_inventory_item_details ADD CONSTRAINT farm_iid_farm_ic_fk FOREIGN KEY (inventory_item_code) REFERENCES farms.farm_inventory_item_codes(inventory_item_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE farms.farm_inventory_item_details ADD CONSTRAINT farm_iid_farm_mscc_fk FOREIGN KEY (multi_stage_commdty_code) REFERENCES farms.farm_multi_stage_commdty_codes(multi_stage_commdty_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE farms.farm_line_items ADD CONSTRAINT farm_li_farm_ctc_fk FOREIGN KEY (commodity_type_code) REFERENCES farms.farm_commodity_type_codes(commodity_type_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE farms.farm_line_items ADD CONSTRAINT farm_li_farm_fvtc_fk FOREIGN KEY (fruit_veg_type_code) REFERENCES farms.farm_fruit_veg_type_codes(fruit_veg_type_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
 ALTER TABLE farms.farm_structure_group_attributs ADD CONSTRAINT farm_sga_farm_iic_fk FOREIGN KEY (structure_group_code) REFERENCES farms.farm_structure_group_codes(structure_group_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE farms.farm_structure_group_attributs ADD CONSTRAINT farm_sga_farm_riic_fk FOREIGN KEY (rollup_structure_group_code) REFERENCES farms.farm_structure_group_codes(structure_group_code) ON DELETE NO ACTION DEFERRABLE INITIALLY IMMEDIATE;
