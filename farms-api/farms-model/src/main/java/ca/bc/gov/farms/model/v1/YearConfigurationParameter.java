367c96bd16b42f1e0ef63d0fed51f177c19045d4
Hong-Yi Wang <hwang@vividsolutions.com>
2025-09-22 11:05:56 -0700

    AS-3274: Implement YearConfigurationParameter.

---
+++ farms-api/farms-model/src/main/java/ca/bc/gov/farms/model/v1/YearConfigurationParameter.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.farms.model.v1;
+
+import java.io.Serializable;
+
+public interface YearConfigurationParameter extends Serializable {
+
+    public Long getYearConfigurationParameterId();
+    public void setYearConfigurationParameterId(Long yearConfigurationParameterId);
+
+    public Integer getProgramYear();
+    public void setProgramYear(Integer programYear);
+
+    public String getParameterName();
+    public void setParameterName(String parameterName);
+
+    public String getParameterValue();
+    public void setParameterValue(String parameterValue);
+
+    public String getConfigParamTypeCode();
+    public void setConfigParamTypeCode(String configParamTypeCode);
+}
