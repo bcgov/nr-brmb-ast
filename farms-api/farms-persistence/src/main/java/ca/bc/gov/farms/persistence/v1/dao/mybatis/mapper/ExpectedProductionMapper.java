77ab67ec3d9230843c5c3136c808ed1a58307bc7
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-02 14:02:24 -0700

    AS-3339: Modify ExpectedProductionMapper.

--- farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/mapper/ExpectedProductionMapper.java
+++ farms-api/farms-persistence/src/main/java/ca/bc/gov/farms/persistence/v1/dao/mybatis/mapper/ExpectedProductionMapper.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.farms.persistence.v1.dao.mybatis.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import ca.bc.gov.farms.persistence.v1.dto.ExpectedProductionDto;
@@ -9,6 +10,8 @@ public interface ExpectedProductionMapper {
     ExpectedProductionDto fetch(Map<String, Object> parameters);
 
     ExpectedProductionDto fetchByInventoryItemCode(Map<String, Object> parameters);
+
+    List<ExpectedProductionDto> fetchAll();
 
     int insertExpectedProduction(Map<String, Object> parameters);
 
