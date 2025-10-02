133edc498949d41b0ccb3f7f5cc21d6dab1d6bea
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-02 10:57:54 -0700

    AS-3277: Modify FruitVegTypeDetail.

--- farms-api/farms-model/src/main/java/ca/bc/gov/farms/model/v1/FruitVegTypeDetail.java
+++ farms-api/farms-model/src/main/java/ca/bc/gov/farms/model/v1/FruitVegTypeDetail.java
@@ -2,17 +2,24 @@ package ca.bc.gov.farms.model.v1;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 public interface FruitVegTypeDetail extends Serializable {
 
-    public Long getFruitVegTypeDetailId();
-    public void setFruitVegTypeDetailId(Long fruitVegTypeDetailId);
+    public String getFruitVegTypeCode();
+    public void setFruitVegTypeCode(String fruitVegTypeCode);
+
+    public String getFruitVegTypeDesc();
+    public void setFruitVegTypeDesc(String fruitVegTypeDesc);
+
+    public Date getEstablishedDate();
+    public void setEstablishedDate(Date establishedDate);
+
+    public Date getExpiryDate();
+    public void setExpiryDate(Date expiryDate);
 
     public BigDecimal getRevenueVarianceLimit();
     public void setRevenueVarianceLimit(BigDecimal revenueVarianceLimit);
-
-    public String getFruitVegTypeCode();
-    public void setFruitVegTypeCode(String fruitVegTypeCode);
 
     public String getUserEmail();
     public void setUserEmail(String userEmail);
