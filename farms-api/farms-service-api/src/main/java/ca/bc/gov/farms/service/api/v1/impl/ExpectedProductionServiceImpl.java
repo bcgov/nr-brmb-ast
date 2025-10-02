819a1743c8e0e2579d2d006238ca951075cbabe1
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-02 14:10:26 -0700

    AS-3339: Modify ExpectedProductionServiceImpl.

--- farms-api/farms-service-api/src/main/java/ca/bc/gov/farms/service/api/v1/impl/ExpectedProductionServiceImpl.java
+++ farms-api/farms-service-api/src/main/java/ca/bc/gov/farms/service/api/v1/impl/ExpectedProductionServiceImpl.java
@@ -14,6 +14,7 @@ import ca.bc.gov.brmb.common.service.api.ValidationFailureException;
 import ca.bc.gov.brmb.common.service.api.code.UserUtil;
 import ca.bc.gov.brmb.common.service.api.model.factory.FactoryContext;
 import ca.bc.gov.farms.model.v1.ExpectedProduction;
+import ca.bc.gov.farms.model.v1.ExpectedProductionList;
 import ca.bc.gov.farms.persistence.v1.dao.ExpectedProductionDao;
 import ca.bc.gov.farms.persistence.v1.dto.ExpectedProductionDto;
 import ca.bc.gov.farms.service.api.v1.ExpectedProductionService;
@@ -48,6 +49,25 @@ public class ExpectedProductionServiceImpl implements ExpectedProductionService 
 
     public void setExpectedProductionDao(ExpectedProductionDao expectedProductionDao) {
         this.expectedProductionDao = expectedProductionDao;
+    }
+
+    @Override
+    public ExpectedProductionList<? extends ExpectedProduction> getAllExpectedProductions(FactoryContext factoryContext)
+            throws ServiceException {
+        logger.debug("<getAllExpectedProductions");
+
+        ExpectedProductionList<? extends ExpectedProduction> result = null;
+
+        try {
+            List<ExpectedProductionDto> dtos = expectedProductionDao.fetchAll();
+
+            result = expectedProductionFactory.getExpectedProductionList(dtos, factoryContext);
+        } catch (Exception e) {
+            throw new ServiceException("DAO threw an exception", e);
+        }
+
+        logger.debug(">getAllExpectedProductions");
+        return result;
     }
 
     @Override
