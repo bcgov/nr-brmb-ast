b57985ca25b8304223b7d19d7b115e483809c55e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-07 15:05:01 -0700

    AS-3205: Add code table endpoints test farm_municipality_codes.

--- farms-api/farms-api-rest-endpoints/src/test/java/ca/bc/gov/farms/api/rest/v1/endpoints/CodeTableEndpointsTest.java
+++ farms-api/farms-api-rest-endpoints/src/test/java/ca/bc/gov/farms/api/rest/v1/endpoints/CodeTableEndpointsTest.java
@@ -73,7 +73,8 @@ public class CodeTableEndpointsTest extends JerseyTest {
             "'farm_inventory_group_codes', 'farm_inventory_group_codes'",
             "'farm_inventory_item_codes', 'farm_inventory_item_codes'",
             "'farm_message_type_codes', 'farm_message_type_codes'",
-            "'farm_multi_stage_commdty_codes', 'farm_multi_stage_commdty_codes'"
+            "'farm_multi_stage_commdty_codes', 'farm_multi_stage_commdty_codes'",
+            "'farm_municipality_codes', 'farm_municipality_codes'"
     })
     public void testGetCodeTable(String codeTableName, String codeTableDescriptiveName) throws Exception {
         Response response = target("/codeTables/" + codeTableName).request().get();
