c84857c6b98c80b06bb8cb5042be161df6dfe92b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-14 14:07:26 -0700

    AS-3286: Implement YearConfigurationParameterEndpointsTest.

---
+++ farms-api/farms-api-rest-endpoints/src/test/java/ca/bc/gov/farms/api/rest/v1/endpoints/YearConfigurationParameterEndpointsTest.java
@@ -0,0 +1,88 @@
+package ca.bc.gov.farms.api.rest.v1.endpoints;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ca.bc.gov.brmb.common.api.rest.code.endpoints.spring.CodeEndpointsSpringConfig;
+import ca.bc.gov.farms.api.rest.v1.endpoints.impl.YearConfigurationParameterEndpointsImpl;
+import ca.bc.gov.farms.api.rest.v1.resource.YearConfigurationParameterRsrc;
+import ca.bc.gov.farms.api.rest.v1.spring.EndpointsSpringConfigTest;
+import ca.bc.gov.farms.api.rest.v1.spring.ResourceFactorySpringConfig;
+import ca.bc.gov.farms.service.api.v1.spring.ServiceApiSpringConfig;
+
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONObject;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class YearConfigurationParameterEndpointsTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+        when(mockRequest.getHeader("X-Forwarded-Proto")).thenReturn("http");
+        when(mockRequest.getHeader("X-Forwarded-Host")).thenReturn("localhost");
+        when(mockRequest.getHeader("If-Match")).thenReturn("*");
+
+        // Load Spring context manually
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(EndpointsSpringConfigTest.class,
+                ServiceApiSpringConfig.class, ResourceFactorySpringConfig.class, CodeEndpointsSpringConfig.class);
+
+        // Create Jersey ResourceConfig and integrate Spring context
+        ResourceConfig config = new ResourceConfig();
+        config.register(ObjectMapperContextResolver.class);
+        config.register(YearConfigurationParameterEndpointsImpl.class);
+        config.property("contextConfig", applicationContext);
+
+        // Bind the mockRequest so it can be injected
+        config.register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(mockRequest).to(HttpServletRequest.class);
+            }
+        });
+
+        return config;
+    }
+
+    @Test
+    @Order(1)
+    public void testCreateYearConfigurationParameter() throws Exception {
+        YearConfigurationParameterRsrc yearConfigurationParameter = new YearConfigurationParameterRsrc();
+        yearConfigurationParameter.setProgramYear(2023);
+        yearConfigurationParameter.setParameterName("Payment Limitation - Percentage of Total Margin Decline");
+        yearConfigurationParameter.setParameterValue("70");
+        yearConfigurationParameter.setConfigParamTypeCode("DECIMAL");
+        yearConfigurationParameter.setUserEmail("jsmith@gmail.com");
+
+        Response response = target("/yearConfigurationParameters").request()
+                .post(Entity.json(yearConfigurationParameter));
+        assertEquals(201, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        assertEquals("YearConfigurationParameterRsrc", jsonObject.getString("@type"));
+        assertEquals(241, jsonObject.getInt("yearConfigurationParameterId"));
+        assertEquals(2023, jsonObject.getInt("programYear"));
+        assertEquals("Payment Limitation - Percentage of Total Margin Decline", jsonObject.getString("parameterName"));
+        assertEquals("70", jsonObject.getString("parameterValue"));
+        assertEquals("DECIMAL", jsonObject.getString("configParamTypeCode"));
+        assertEquals("null", jsonObject.getString("userEmail"));
+    }
+}
