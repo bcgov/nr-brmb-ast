61ee41837706b7cb2b94273aa6c12b04b741b6ca
hwang-vividsolutions <hwang@vividsolutions.com>
2025-10-21 09:07:18 -0700

    Feature/as 3285 unit test endpoint for configuration parameter (#58)
    * AS-3285: Implement ConfigurationParameterEndpointsTest.
    
    * AS-3285: Implement ConfigurationParameterEndpointsTest.
    
    * AS-3285: Implement ConfigurationParameterEndpointsTest.
    
    * AS-3285: Implement ConfigurationParameterEndpointsTest.
    
    * AS-3285: Implement ConfigurationParameterEndpointsTest.
    
    * AS-3285: Implement ConfigurationParameterEndpointsTest.

---
+++ farms-api/farms-api-rest-endpoints/src/test/java/ca/bc/gov/farms/api/rest/v1/endpoints/ConfigurationParameterEndpointsTest.java
@@ -0,0 +1,182 @@
+package ca.bc.gov.farms.api.rest.v1.endpoints;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.Response;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ca.bc.gov.brmb.common.api.rest.code.endpoints.spring.CodeEndpointsSpringConfig;
+import ca.bc.gov.farms.api.rest.v1.endpoints.impl.ConfigurationParameterEndpointsImpl;
+import ca.bc.gov.farms.api.rest.v1.resource.ConfigurationParameterRsrc;
+import ca.bc.gov.farms.api.rest.v1.spring.EndpointsSpringConfigTest;
+import ca.bc.gov.farms.api.rest.v1.spring.ResourceFactorySpringConfig;
+import ca.bc.gov.farms.service.api.v1.spring.ServiceApiSpringConfig;
+
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ConfigurationParameterEndpointsTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+        when(mockRequest.getHeader("X-Forwarded-Proto")).thenReturn("http");
+        when(mockRequest.getHeader("X-Forwarded-Host")).thenReturn("localhost");
+        when(mockRequest.getHeader("If-Match")).thenReturn("*");
+
+        // Load Spring context manually
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(EndpointsSpringConfigTest.class,
+                ServiceApiSpringConfig.class, ResourceFactorySpringConfig.class, CodeEndpointsSpringConfig.class);
+
+        // Create Jersey ResourceConfig and integrate Spring context
+        ResourceConfig config = new ResourceConfig();
+        config.register(ObjectMapperContextResolver.class);
+        config.register(ConfigurationParameterEndpointsImpl.class);
+        config.property("contextConfig", applicationContext);
+
+        // Bind the mockRequest so it can be injected
+        config.register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(mockRequest).to(HttpServletRequest.class);
+            }
+        });
+
+        return config;
+    }
+
+    @Test
+    @Order(1)
+    public void testCreateConfigurationParameter() throws Exception {
+        ConfigurationParameterRsrc configurationParameter = new ConfigurationParameterRsrc();
+        configurationParameter.setParameterName("CDOGS - Api Version");
+        configurationParameter.setParameterValue("2");
+        configurationParameter.setSensitiveDataInd("N");
+        configurationParameter.setConfigParamTypeCode("STRING");
+        configurationParameter.setUserEmail("jsmith@gmail.com");
+
+        Response response = target("/configurationParameters").request()
+                .post(Entity.json(configurationParameter));
+        assertEquals(201, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        assertEquals("ConfigurationParameterRsrc", jsonObject.getString("@type"));
+        assertEquals(481, jsonObject.getInt("configurationParameterId"));
+        assertEquals("CDOGS - Api Version", jsonObject.getString("parameterName"));
+        assertEquals("2", jsonObject.getString("parameterValue"));
+        assertEquals("N", jsonObject.getString("sensitiveDataInd"));
+        assertEquals("STRING", jsonObject.getString("configParamTypeCode"));
+        assertEquals("null", jsonObject.getString("userEmail"));
+    }
+
+    @Test
+    @Order(2)
+    public void testGetAllConfigurationParameters() throws Exception {
+        Response response = target("/configurationParameters").request().get();
+        assertEquals(200, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        assertEquals("ConfigurationParameterListRsrc", jsonObject.getString("@type"));
+
+        JSONArray configurationParameterList = jsonObject.getJSONArray("configurationParameterList");
+        JSONObject configurationParameter = configurationParameterList.getJSONObject(0);
+        assertEquals("ConfigurationParameterRsrc", configurationParameter.getString("@type"));
+        assertEquals(481, configurationParameter.getInt("configurationParameterId"));
+        assertEquals("CDOGS - Api Version", configurationParameter.getString("parameterName"));
+        assertEquals("2", configurationParameter.getString("parameterValue"));
+        assertEquals("N", configurationParameter.getString("sensitiveDataInd"));
+        assertEquals("STRING", configurationParameter.getString("configParamTypeCode"));
+        assertEquals("null", configurationParameter.getString("userEmail"));
+    }
+
+    @Test
+    @Order(3)
+    public void testGetAllConfigurationParameters1() throws Exception {
+        Response response = target("/configurationParameters").queryParam("nameStartsWith", "CDOGS").request().get();
+        assertEquals(200, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        assertEquals("ConfigurationParameterListRsrc", jsonObject.getString("@type"));
+
+        JSONArray configurationParameterList = jsonObject.getJSONArray("configurationParameterList");
+        JSONObject configurationParameter = configurationParameterList.getJSONObject(0);
+        assertEquals("ConfigurationParameterRsrc", configurationParameter.getString("@type"));
+        assertEquals(481, configurationParameter.getInt("configurationParameterId"));
+        assertEquals("CDOGS - Api Version", configurationParameter.getString("parameterName"));
+        assertEquals("2", configurationParameter.getString("parameterValue"));
+        assertEquals("N", configurationParameter.getString("sensitiveDataInd"));
+        assertEquals("STRING", configurationParameter.getString("configParamTypeCode"));
+        assertEquals("null", configurationParameter.getString("userEmail"));
+    }
+
+    @Test
+    @Order(4)
+    public void testGetConfigurationParameter() throws Exception {
+        Response response = target("/configurationParameters/481").request().get();
+        assertEquals(200, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        assertEquals("ConfigurationParameterRsrc", jsonObject.getString("@type"));
+        assertEquals(481, jsonObject.getInt("configurationParameterId"));
+        assertEquals("CDOGS - Api Version", jsonObject.getString("parameterName"));
+        assertEquals("2", jsonObject.getString("parameterValue"));
+        assertEquals("N", jsonObject.getString("sensitiveDataInd"));
+        assertEquals("STRING", jsonObject.getString("configParamTypeCode"));
+        assertEquals("null", jsonObject.getString("userEmail"));
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateConfigurationParameter() throws Exception {
+        ConfigurationParameterRsrc configurationParameter = new ConfigurationParameterRsrc();
+        configurationParameter.setConfigurationParameterId(481L);
+        configurationParameter.setParameterName("CDOGS - Api Version");
+        configurationParameter.setParameterValue("3");
+        configurationParameter.setSensitiveDataInd("N");
+        configurationParameter.setConfigParamTypeCode("STRING");
+        configurationParameter.setUserEmail("jsmith@gmail.com");
+
+        Response response = target("/configurationParameters/481").request().put(Entity.json(configurationParameter));
+        assertEquals(200, response.getStatus());
+
+        String jsonString = response.readEntity(String.class);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        assertEquals("ConfigurationParameterRsrc", jsonObject.getString("@type"));
+        assertEquals(481, jsonObject.getInt("configurationParameterId"));
+        assertEquals("CDOGS - Api Version", jsonObject.getString("parameterName"));
+        assertEquals("3", jsonObject.getString("parameterValue"));
+        assertEquals("N", jsonObject.getString("sensitiveDataInd"));
+        assertEquals("STRING", jsonObject.getString("configParamTypeCode"));
+        assertEquals("null", jsonObject.getString("userEmail"));
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteConfigurationParameter() throws Exception {
+        Response response = target("/configurationParameters/481").request().delete();
+        assertEquals(204, response.getStatus());
+    }
+}
