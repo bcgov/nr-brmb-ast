4c2158b77cc2c43dd2d809c4fed5d8cc7a78bca4
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-21 10:50:14 -0700

    AS-3361: Implement farms_search_pkg.search_accounts_by_pin.

--- farms-liquibase/scripts/01_00_xx/01_00_03/05/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/05/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON FUNCTION farms_search_pkg.search_imports TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_accounts_by_pin TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_search_pkg TO "app_farms_rest_proxy";
