f0b59cc6c36e98e7f67ced974040d5ae0f63dde9
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 13:59:54 -0700

    AS-3351: Implement farms_calculator_pkg.read_operations_for_puc_import.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,5 +1,6 @@
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_state TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_category TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_participnt_data_src_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_calculator_pkg.read_operations_for_puc_import TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
