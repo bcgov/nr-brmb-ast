afd6b6e7ebd14379f92299e54909edca3be8e5ff
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 16:35:25 -0700

    AS-3351: Implement farms_types_pkg.line_item_not_found_msg.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -26,6 +26,7 @@ GRANT EXECUTE ON FUNCTION farms_types_pkg.xref_more_than_one_found_msg TO "app_f
 GRANT EXECUTE ON FUNCTION farms_types_pkg.xref_not_found_num TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.xref_not_found_msg TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.line_item_not_found_num TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_types_pkg.line_item_not_found_msg TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_types_pkg TO "app_farms_rest_proxy";
