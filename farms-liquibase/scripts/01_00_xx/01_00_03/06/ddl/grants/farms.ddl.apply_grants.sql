b0a1a9fff764697b6bb23b0542542952ade59a7b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 13:39:29 -0700

    AS-3351: Implement farms_calculator_pkg.update_scenario_participnt_data_src_code.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_state TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_category TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_participnt_data_src_code TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
