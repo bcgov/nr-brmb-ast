1224e0c2c661c803dcd82ed7d374f3c4b23a46b7
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 15:46:55 -0700

    AS-3351: Implement farms_types_pkg.invalid_revision_count_code.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -9,6 +9,7 @@ GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_all_sc_rev TO "app_farms_
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_msg TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.revision_count_increment TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_types_pkg.invalid_revision_count_code TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
 GRANT USAGE ON SCHEMA farms_types_pkg TO "app_farms_rest_proxy";
