45c9aa4a0604c9dc4cf73d17b6241595925c6dd2
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 15:32:42 -0700

    AS-3351: Implement farms_types_pkg.data_not_current_msg.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -7,5 +7,6 @@ GRANT EXECUTE ON FUNCTION farms_calculator_pkg.read_operations_for_accrual_impor
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_all_sc_rev TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_code TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_msg TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
