b3b6f989269c4f68ad3b3ae85167d449cb95367c
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 14:30:21 -0700

    AS-3351: Add indentation.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/routines/farms_calculator_pkg.read_operations_for_puc_import.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/routines/farms_calculator_pkg.read_operations_for_puc_import.sql
@@ -12,54 +12,54 @@ declare
 
 begin
     open cur for
-    select sv.program_year_version_number,
-           sv.scenario_number,
-           sv.scenario_class_code,
-           stc.description as scenario_class_desc,
-           sv.scenario_category_code,
-           scc.description scenario_category_code_desc,
-           sv.scenario_state_code,
-           ssc.description as scenario_state_desc,
-           sc.when_created,
-           fo.alignment_key,
-           fo.partnership_pin,
-           fo.partnership_name,
-           fo.farming_operation_id,
-           case when exists (
-           select *
-               from farms.farm_productve_unit_capacities puc
-               where puc.participnt_data_src_code = 'CRA'
-               and puc.farming_operation_id = fo.farming_operation_id
-               and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-           )
-           then 'Y' else 'N' end has_cra_productive_units,
-           case when exists (
-               select *
-               from farms.farm_productve_unit_capacities puc
-               where puc.participnt_data_src_code = 'LOCAL'
-               and puc.farming_operation_id = fo.farming_operation_id
-               and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-            )
-            then 'Y' else 'N' end has_local_productive_units
-    from farms.farm_scenarios_vw sv
-    join farms.farm_agristability_scenarios sc on sc.agristability_scenario_id = sv.agristability_scenario_id
-    join farms.farm_scenario_state_codes ssc on ssc.scenario_state_code = sc.scenario_state_code
-    join farms.farm_scenario_class_codes stc on stc.scenario_class_code = Sc.scenario_class_code
-    join farms.farm_scenario_category_codes scc on scc.scenario_category_code = sc.scenario_category_code
-    join farms.farm_farming_operations fo on fo.program_year_version_id = sv.program_year_version_id
-    where sv.participant_pin = in_participant_pin
-    and sv.year = in_year
-    and sv.program_year_version_number != in_excluding_program_year_version_number
-    and exists (
-        select *
-        from farms.farm_productve_unit_capacities puc
-        where puc.farming_operation_id = fo.farming_operation_id
-        and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-    )
-    order by sv.year desc,
-          sv.program_year_version_number desc,
-          sv.scenario_number desc,
-          fo.alignment_key;
+        select sv.program_year_version_number,
+               sv.scenario_number,
+               sv.scenario_class_code,
+               stc.description as scenario_class_desc,
+               sv.scenario_category_code,
+               scc.description scenario_category_code_desc,
+               sv.scenario_state_code,
+               ssc.description as scenario_state_desc,
+               sc.when_created,
+               fo.alignment_key,
+               fo.partnership_pin,
+               fo.partnership_name,
+               fo.farming_operation_id,
+               case when exists (
+                   select *
+                   from farms.farm_productve_unit_capacities puc
+                   where puc.participnt_data_src_code = 'CRA'
+                   and puc.farming_operation_id = fo.farming_operation_id
+                   and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
+               )
+               then 'Y' else 'N' end has_cra_productive_units,
+               case when exists (
+                   select *
+                   from farms.farm_productve_unit_capacities puc
+                   where puc.participnt_data_src_code = 'LOCAL'
+                   and puc.farming_operation_id = fo.farming_operation_id
+                   and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
+               )
+               then 'Y' else 'N' end has_local_productive_units
+        from farms.farm_scenarios_vw sv
+        join farms.farm_agristability_scenarios sc on sc.agristability_scenario_id = sv.agristability_scenario_id
+        join farms.farm_scenario_state_codes ssc on ssc.scenario_state_code = sc.scenario_state_code
+        join farms.farm_scenario_class_codes stc on stc.scenario_class_code = Sc.scenario_class_code
+        join farms.farm_scenario_category_codes scc on scc.scenario_category_code = sc.scenario_category_code
+        join farms.farm_farming_operations fo on fo.program_year_version_id = sv.program_year_version_id
+        where sv.participant_pin = in_participant_pin
+        and sv.year = in_year
+        and sv.program_year_version_number != in_excluding_program_year_version_number
+        and exists (
+            select *
+            from farms.farm_productve_unit_capacities puc
+            where puc.farming_operation_id = fo.farming_operation_id
+            and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
+        )
+        order by sv.year desc,
+                 sv.program_year_version_number desc,
+                 sv.scenario_number desc,
+                 fo.alignment_key;
 
     return cur;
 end;
