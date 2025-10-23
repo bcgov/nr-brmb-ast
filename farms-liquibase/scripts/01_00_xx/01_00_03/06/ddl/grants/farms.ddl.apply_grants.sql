c74d9602c44d31ce6c44b5d5db28e8cfcd3bb51e
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-23 14:42:03 -0700

    AS-3351: Implement farms_calculator_pkg.read_operations_for_accrual_import.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -3,5 +3,6 @@ GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_category TO "app
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_scenario_participnt_data_src_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_calculator_pkg.read_operations_for_puc_import TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_calculator_pkg.read_operations_for_inventory_import TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_calculator_pkg.read_operations_for_accrual_import TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_calculator_pkg TO "app_farms_rest_proxy";
--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/routines/farms_calculator_pkg.read_operations_for_accrual_import.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/routines/farms_calculator_pkg.read_operations_for_accrual_import.sql
@@ -1,4 +1,4 @@
-create or replace function farms_calculator_pkg.read_operations_for_puc_import(
+create or replace function farms_calculator_pkg.read_operations_for_accrual_import(
     in in_participant_pin farms.farm_scenarios_vw.participant_pin%type,
     in in_year farms.farm_scenarios_vw.year%type,
     in in_excluding_program_year_version_number farms.farm_scenarios_vw.program_year_version_number%type
@@ -24,23 +24,7 @@ begin
                fo.alignment_key,
                fo.partnership_pin,
                fo.partnership_name,
-               fo.farming_operation_id,
-               case when exists (
-                   select *
-                   from farms.farm_productve_unit_capacities puc
-                   where puc.participnt_data_src_code = 'CRA'
-                   and puc.farming_operation_id = fo.farming_operation_id
-                   and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-               )
-               then 'Y' else 'N' end has_cra_productive_units,
-               case when exists (
-                   select *
-                   from farms.farm_productve_unit_capacities puc
-                   where puc.participnt_data_src_code = 'LOCAL'
-                   and puc.farming_operation_id = fo.farming_operation_id
-                   and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-               )
-               then 'Y' else 'N' end has_local_productive_units
+               fo.farming_operation_id
         from farms.farm_scenarios_vw sv
         join farms.farm_agristability_scenarios sc on sc.agristability_scenario_id = sv.agristability_scenario_id
         join farms.farm_scenario_state_codes ssc on ssc.scenario_state_code = sc.scenario_state_code
@@ -52,10 +36,13 @@ begin
         and sv.program_year_version_number != in_excluding_program_year_version_number
         and exists (
             select *
-            from farms.farm_productve_unit_capacities puc
-            where puc.farming_operation_id = fo.farming_operation_id
-            and (coalesce(puc.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and puc.agristability_scenario_id = sv.agristability_scenario_id))
-        )
+            from farms.farm_reported_inventories ri
+            join farms.farm_agristabilty_cmmdty_xref acx on acx.agristabilty_cmmdty_xref_id = ri.agristabilty_cmmdty_xref_id
+            where ri.farming_operation_id = fo.farming_operation_id
+            and (coalesce(ri.agristability_scenario_id::text, '') = '' or (sv.scenario_class_code in ('USER','REF') and ri.agristability_scenario_id = sv.agristability_scenario_id))
+            and acx.inventory_class_code in ('3', '4', '5')
+            and (coalesce(ri.start_of_year_amount, 0) != 0 or coalesce(ri.end_of_year_amount, 0) != 0) 
+        ) 
         order by sv.year desc,
                  sv.program_year_version_number desc,
                  sv.scenario_number desc,
