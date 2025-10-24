486f621b6457b6209507c62212b65326eb161419
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-24 16:18:01 -0700

    AS-3351: Implement farms_calculator_pkg.update_non_participant_ind.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -28,6 +28,7 @@ GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.copy_adjustments TO "app_farms_r
 GRANT EXECUTE ON FUNCTION farms_calculator_pkg.create_ref_scenario TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.assign_to_user TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_cash_margins_ind TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_non_participant_ind TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_msg TO "app_farms_rest_proxy";
