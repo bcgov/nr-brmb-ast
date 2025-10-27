0713c71e575ecfba951b13cbd546cbdfff9bac98
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-27 10:28:52 -0700

    AS-3351: Implement farms_calculator_pkg.update_pyv_copy_old.

--- farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/06/ddl/grants/farms.ddl.apply_grants.sql
@@ -31,6 +31,7 @@ GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_cash_margins_ind TO "app_
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_non_participant_ind TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_late_participant_ind TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_calculator_pkg.save_scenario_as_new TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_calculator_pkg.update_pyv_copy_old TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_code TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_types_pkg.data_not_current_msg TO "app_farms_rest_proxy";
