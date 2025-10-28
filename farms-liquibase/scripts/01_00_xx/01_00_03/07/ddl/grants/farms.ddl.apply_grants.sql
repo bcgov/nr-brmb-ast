4803e722283e96f1a15b625f350a86a1a6632ef9
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-28 15:46:14 -0700

    AS-3359: Implement farms_webapp_pkg.import_in_progress_check.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -26,3 +26,4 @@ GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_line_items TO "app_farms_rest_pro
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_deduction_line_items TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_inventory_valid_items TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_scheduled_job TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_webapp_pkg.import_in_progress_check TO "app_farms_rest_proxy";
