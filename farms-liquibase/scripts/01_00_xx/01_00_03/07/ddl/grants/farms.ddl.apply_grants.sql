2066f5000b40f2e13bc3ba4bcf4af443d0fb79bc
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-28 11:44:31 -0700

    AS-3359: Implement farms_webapp_pkg.get_inventory_class_codes.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_encryption_key TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.encrypt(text) TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.decrypt(text) TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_inventory_class_codes TO "app_farms_rest_proxy";
