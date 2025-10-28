e41927add12ebc203e48186b56a0eea900b00124
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-28 13:01:25 -0700

    AS-3359: Implement farms_webapp_pkg.get_regional_office_codes.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -4,3 +4,4 @@ GRANT EXECUTE ON FUNCTION farms_webapp_pkg.decrypt(text) TO "app_farms_rest_prox
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_inventory_class_codes TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_inventory_group_codes TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_export_class_codes TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_regional_office_codes TO "app_farms_rest_proxy";
