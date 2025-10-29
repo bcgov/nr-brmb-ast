a9644f58f6f7c11de24e20dc680477ac84b4d5db
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-29 14:45:35 -0700

    AS-3359: Implement farms_webapp_pkg.get_verifed_sc_cf_number.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -53,3 +53,4 @@ GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_contact_transfer TO "app_farms_r
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_contact_transfer_program_year_version TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_combined_farm_ip_sc TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_ip_sc_combined_farm_number TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_verifed_sc_cf_number TO "app_farms_rest_proxy";
