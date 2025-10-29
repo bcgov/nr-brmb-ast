e6afae4f4eaaf357b10b04b5aa68b7fbdf0f3be9
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-29 14:04:48 -0700

    AS-3359: Implement farms_webapp_pkg.read_contact_transfer.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -49,3 +49,4 @@ GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_ref_year_claim TO "app_farms_rest
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_inbox TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_sc_rev_count TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.pyv_has_verified_sc TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_contact_transfer TO "app_farms_rest_proxy";
