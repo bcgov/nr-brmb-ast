a553958ee05afe6b17b5b4b07410c3c524b2f70c
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-29 13:02:50 -0700

    AS-3359: Implement farms_webapp_pkg.get_authorized_users.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -32,3 +32,4 @@ GRANT EXECUTE ON PROCEDURE farms_webapp_pkg.confirm_import TO "app_farms_rest_pr
 GRANT EXECUTE ON PROCEDURE farms_webapp_pkg.retry_staging TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_import_version_blob TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_import_version_blob_upd TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_authorized_users TO "app_farms_rest_proxy";
