02e2db797627a1a7a0300b75db94c0d91290f11d
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-29 14:42:40 -0700

    AS-3359: Implement farms_webapp_pkg.get_ip_sc_combined_farm_number.

--- farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/07/ddl/grants/farms.ddl.apply_grants.sql
@@ -52,3 +52,4 @@ GRANT EXECUTE ON FUNCTION farms_webapp_pkg.pyv_has_verified_sc TO "app_farms_res
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_contact_transfer TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.read_contact_transfer_program_year_version TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_combined_farm_ip_sc TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_webapp_pkg.get_ip_sc_combined_farm_number TO "app_farms_rest_proxy";
