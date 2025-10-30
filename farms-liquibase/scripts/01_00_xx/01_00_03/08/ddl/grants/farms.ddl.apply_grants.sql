63f952553e12543ccb9d55b388a3ed752527638c
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-30 10:41:36 -0700

    AS-3348: Implement farms_adjustment_pkg.adjust_income_expense.

--- farms-liquibase/scripts/01_00_xx/01_00_03/08/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/08/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON PROCEDURE farms_adjustment_pkg.adjust_puc TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_adjustment_pkg.adjust_income_expense TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_adjustment_pkg TO "app_farms_rest_proxy";
