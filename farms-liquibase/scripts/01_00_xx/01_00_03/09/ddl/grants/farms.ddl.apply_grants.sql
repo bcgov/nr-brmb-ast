a4fb90af75997459d7c51dade7a484760d429208
Hong-Yi Wang <hwang@vividsolutions.com>
2025-10-31 10:15:52 -0700

    AS-3356: Implement farms_negative_margin_pkg.calculate_negative_margins.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_03/09/ddl/grants/farms.ddl.apply_grants.sql
@@ -0,0 +1,3 @@
+GRANT EXECUTE ON PROCEDURE farms_negative_margin_pkg.calculate_negative_margins TO "app_farms_rest_proxy";
+
+GRANT USAGE ON SCHEMA farms_negative_margin_pkg TO "app_farms_rest_proxy";
