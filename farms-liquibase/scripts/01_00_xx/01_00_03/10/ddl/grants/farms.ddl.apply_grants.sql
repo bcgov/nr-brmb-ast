684eb03455879b9fa664387670bea4e3caf2246f
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-03 09:49:39 -0800

    AS-3357: Implement farms_reasonability_read_pkg.read_reasonability_forage_production_test.

--- farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_tests TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_benefit_risk_pu TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_forage_production_test TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_read_pkg TO "app_farms_rest_proxy";
