39934eef070fc9b9c88ed89ad85319b2f2d6b9ee
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-03 09:45:32 -0800

    AS-3357: Implement farms_reasonability_read_pkg.read_reasonability_benefit_risk_pu.

--- farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_tests TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_benefit_risk_pu TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_read_pkg TO "app_farms_rest_proxy";
