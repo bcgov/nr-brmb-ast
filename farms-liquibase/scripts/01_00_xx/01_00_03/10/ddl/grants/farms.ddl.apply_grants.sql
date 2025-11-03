b5ab8131882948ec713cd02773098db338d2c79c
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-03 10:57:20 -0800

    AS-3357: Implement farms_reasonability_read_pkg.read_farm_rsnblty_rev_pltry_brl_rslts.

--- farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
@@ -13,5 +13,6 @@ GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_farm_rsnblty_rev_fru
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_farm_rsnblty_rev_nrsry_results TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_farm_rsnblty_rev_nrsry_invn_rslts TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_farm_rsnblty_rev_nrsry_incm_rslts TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_farm_rsnblty_rev_pltry_brl_rslts TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_read_pkg TO "app_farms_rest_proxy";
