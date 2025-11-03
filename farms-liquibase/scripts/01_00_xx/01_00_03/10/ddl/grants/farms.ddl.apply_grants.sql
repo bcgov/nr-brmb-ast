645e72c75b29902ab937ef5d2999800247f02608
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-03 09:40:08 -0800

    AS-3357: Implement farms_reasonability_read_pkg.read_reasonability_tests.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_03/10/ddl/grants/farms.ddl.apply_grants.sql
@@ -0,0 +1,3 @@
+GRANT EXECUTE ON FUNCTION farms_reasonability_read_pkg.read_reasonability_tests TO "app_farms_rest_proxy";
+
+GRANT USAGE ON SCHEMA farms_reasonability_read_pkg TO "app_farms_rest_proxy";
