3c52221b089e5a5aa3464050f39cba850f176b64
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-04 11:42:24 -0800

    AS-3358: Implement farms_reasonability_write_pkg.update_farm_rsnblty_rev_g_f_fs_invn_rslts.

--- farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
@@ -6,5 +6,6 @@ GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.create_rsnblty_test_mes
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_rsn_prdctn_frut_invntries TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_rsnblty_prdctn_fruit_veg_rslts TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_farm_rsnblty_prdctn_grain_rslts TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_farm_rsnblty_rev_g_f_fs_invn_rslts TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_write_pkg TO "app_farms_rest_proxy";
