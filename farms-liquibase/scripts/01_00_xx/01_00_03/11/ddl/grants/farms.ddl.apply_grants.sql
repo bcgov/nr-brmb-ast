7a61b3693012123ace7f9cfc195d3b24711fcdf2
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-04 10:04:31 -0800

    AS-3358: Implement farms_reasonability_write_pkg.update_reasonability_tests.

---
+++ farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
@@ -0,0 +1,3 @@
+GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_reasonability_tests TO "app_farms_rest_proxy";
+
+GRANT USAGE ON SCHEMA farms_reasonability_write_pkg TO "app_farms_rest_proxy";
