3d48a248d14280abcb5a6024b07237eb25d8e404
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-04 11:18:48 -0800

    AS-3358: Implement farms_reasonability_write_pkg.create_rsnblty_test_message.

--- farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
@@ -2,5 +2,6 @@ GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_reasonability_te
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.flag_reasonability_tests_stale TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.create_farm_rsnblty_bnft_rsk_pu TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_farm_rsnblty_prdctn_forage_rslts TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.create_rsnblty_test_message TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_write_pkg TO "app_farms_rest_proxy";
