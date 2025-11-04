7e318fcec83ee1ee81b5d2fddfeeaec092d45534
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-04 10:08:53 -0800

    AS-3358: Implement farms_reasonability_write_pkg.flag_reasonability_tests_stale.

--- farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/11/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.update_reasonability_tests TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_reasonability_write_pkg.flag_reasonability_tests_stale TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_reasonability_write_pkg TO "app_farms_rest_proxy";
