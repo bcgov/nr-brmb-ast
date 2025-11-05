fd04a92bfc6316f4e9237d3f5c2e623ad264553b
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-05 13:41:20 -0800

    AS-3360: Implement farms_write_pkg.write_final_verification_notes.

--- farms-liquibase/scripts/01_00_xx/01_00_03/12/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/12/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,5 +1,6 @@
 GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_claim TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_margin TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_margin_total TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_write_pkg.write_final_verification_notes TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_write_pkg TO "app_farms_rest_proxy";
