b47ae357b33017e1b664a75d28ab9ab8eaa5b945
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-05 13:38:04 -0800

    AS-3360: Implement farms_write_pkg.write_margin_total.

--- farms-liquibase/scripts/01_00_xx/01_00_03/12/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/12/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_claim TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_margin TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_write_pkg.write_margin_total TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_write_pkg TO "app_farms_rest_proxy";
