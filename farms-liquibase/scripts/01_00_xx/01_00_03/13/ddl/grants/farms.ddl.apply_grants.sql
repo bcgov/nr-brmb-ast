aa8a8f81217b4fcf16d806461622f11e02401fc8
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-06 16:08:04 -0800

    AS-3350: Implement farms_import_xml_pkg.get_import_top_level_errors.

--- farms-liquibase/scripts/01_00_xx/01_00_03/13/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/13/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON FUNCTION farms_import_xml_pkg.get_import_numbers TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_import_xml_pkg.get_import_top_level_errors TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_import_xml_pkg TO "app_farms_rest_proxy";
