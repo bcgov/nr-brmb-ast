376053b21ba83f239153f73c05f6c4ab6e9be833
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-07 11:18:25 -0800

    AS-3352: Implement farms_enrolment_read_pkg.read_staging.

--- farms-liquibase/scripts/01_00_xx/01_00_03/14/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/14/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON FUNCTION farms_enrolment_read_pkg.read_enrolments TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_enrolment_read_pkg.read_staging_results TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_enrolment_read_pkg.read_staging TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_enrolment_read_pkg TO "app_farms_rest_proxy";
