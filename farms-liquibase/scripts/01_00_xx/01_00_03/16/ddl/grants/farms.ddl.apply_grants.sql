7589e4770a2ac8cb5dbff6c56ea088da7d99f1a4
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-12 09:57:21 -0800

    AS-3093: Implement farms_bpu_pkg.insert_staging_row.

--- farms-liquibase/scripts/01_00_xx/01_00_03/16/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/16/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.staging_to_operational TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_bpu_pkg.insert_staging_row TO "app_farms_rest_proxy";
 
 GRANT TRUNCATE ON TABLE farms.farm_zbpu_benchmark_per_units TO "app_farms_rest_proxy";
