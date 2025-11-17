3144485cd352d5751396ea6915dc518b62d75dc2
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-17 11:49:28 -0800

    AS-3354: Implement farms_export_pkg.f01.

--- farms-liquibase/scripts/01_00_xx/01_00_03/19/ddl/routines/farms_export_pkg.f01.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/19/ddl/routines/farms_export_pkg.f01.sql
@@ -11,34 +11,18 @@ declare
 begin
 
     open cur for
-        select ac.participant_pin,
-               py.year program_year,
-               pyv.province_of_residence,
-               pyv.province_of_main_farmstead,
-               ac.participant_class_code,
-               pyv.post_mark_date,
-               pyv.received_date,
-               pyv.sole_proprietor_ind,
-               pyv.partnership_member_ind,
-               pyv.corporate_shareholder_ind,
-               pyv.coop_member_ind,
-               pyv.common_share_total,
-               pyv.farm_years,
-               pyv.last_year_farming_ind
-        from farms.farm_agristability_clients ac
-        join farms.farm_program_years py on py.agristability_client_id = ac.agristability_client_id
-        join farms.farm_program_year_versions pyv on pyv.program_year_id = py.program_year_id
-        join (
+        with ranked as (
             /* this subquery is duplicated in F01, F02, F03, F20, F21, F30, F31, F40, F60.
              * if it is modified here, it should be modified there as well.
              */
             select m.program_year_version_id,
                    sc.agristability_scenario_id,
+                   m.program_year_id,
                    m.participant_pin,
-                   m.year program_year,
+                   m.year as program_year,
                    sc.combined_farm_number,
                    ssa.scenario_state_audit_id,
-                   first_value(sc.agristability_scenario_id) over(
+                   dense_rank() over(
                        partition by m.program_year_id
                        order by case when(sc.scenario_state_code = 'COMP') then 0 else 1 end,
                                 case when(sc.scenario_state_code = 'AMEND') then 0 else 1 end,
@@ -57,7 +41,7 @@ begin
                                 end desc,
                                 ssa.when_created desc,
                                 sc.scenario_number desc
-                   ) latest_sc_id,
+                   ) as rnk,
                    first_value(ssa.scenario_state_audit_id) over (
                        partition by sc.agristability_scenario_id
                        order by ssa.when_created desc nulls last
@@ -74,8 +58,37 @@ begin
             left outer join farms.farm_scenario_state_audits ssa on ssa.agristability_scenario_id = sc.agristability_scenario_id
                                                                  and ssa.scenario_state_code = 'COMP'
             where m.year = in_program_year
+        ), chosen as (
+            select distinct on (program_year_id)
+                   program_year_id,
+                   min(agristability_scenario_id) as latest_sc_id
+            from ranked
+            where rnk = 1
+            group by program_year_id
+        )
+        select ac.participant_pin,
+               py.year program_year,
+               pyv.province_of_residence,
+               pyv.province_of_main_farmstead,
+               ac.participant_class_code,
+               pyv.post_mark_date,
+               pyv.received_date,
+               pyv.sole_proprietor_ind,
+               pyv.partnership_member_ind,
+               pyv.corporate_shareholder_ind,
+               pyv.coop_member_ind,
+               pyv.common_share_total,
+               pyv.farm_years,
+               pyv.last_year_farming_ind
+        from farms.farm_agristability_clients ac
+        join farms.farm_program_years py on py.agristability_client_id = ac.agristability_client_id
+        join farms.farm_program_year_versions pyv on pyv.program_year_id = py.program_year_id
+        join (
+            select r.*
+            from ranked r
+            join chosen c on r.program_year_id = c.program_year_id
+                          and r.agristability_scenario_id = c.latest_sc_id
         ) t on pyv.program_year_version_id = t.program_year_version_id
-            and t.agristability_scenario_id = t.latest_sc_id
             and(t.scenario_state_audit_id = t.verified_state_id or coalesce(t.verified_state_id::text, '') = '')
         where py.non_participant_ind = 'N'
         order by ac.participant_pin;
