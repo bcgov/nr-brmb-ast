c580edd0e27fb56f0dc691aea87465cd3ba64bd8
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-18 13:41:05 -0800

    AS-3354: Implement farms_export_pkg.f03_statement_a.

--- farms-liquibase/scripts/01_00_xx/01_00_03/19/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/19/ddl/grants/farms.ddl.apply_grants.sql
@@ -29,5 +29,6 @@ GRANT EXECUTE ON FUNCTION farms_export_pkg.f60_ade TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_export_pkg.detailed_scenario_extract TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_export_pkg.f01_statement_a TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_export_pkg.f02_statement_a TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_export_pkg.f03_statement_a TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_export_pkg TO "app_farms_rest_proxy";
