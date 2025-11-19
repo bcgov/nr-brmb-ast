c440284a6692058112eff3b8fddb4ce8e64ee698
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-19 15:26:50 -0800

    AS-3353: Implement farms_enrolment_write_pkg.upsert_operational.

--- farms-liquibase/scripts/01_00_xx/01_00_03/20/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/20/ddl/grants/farms.ddl.apply_grants.sql
@@ -5,5 +5,6 @@ GRANT EXECUTE ON PROCEDURE farms_enrolment_write_pkg.generate_non_enw_enrolment 
 GRANT EXECUTE ON FUNCTION farms_enrolment_write_pkg.generate_enrolments TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_enrolment_write_pkg.update_staging TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_enrolment_write_pkg.copy_staging_to_operational TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_enrolment_write_pkg.upsert_operational TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_enrolment_write_pkg TO "app_farms_rest_proxy";
