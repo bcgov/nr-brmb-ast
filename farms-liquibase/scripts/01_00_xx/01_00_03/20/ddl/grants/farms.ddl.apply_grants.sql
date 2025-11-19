b876b8657e118eede550a77be58772d4fd4c7c84
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-19 11:30:03 -0800

    AS-3353: Implement farms_enrolment_write_pkg.generate_non_enw_enrolment.

--- farms-liquibase/scripts/01_00_xx/01_00_03/20/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/20/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,5 +1,6 @@
 GRANT EXECUTE ON FUNCTION farms_read_pkg.read_py_id TO "app_farms_rest_proxy";
 
 GRANT EXECUTE ON FUNCTION farms_enrolment_write_pkg.get_scenario_id TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_enrolment_write_pkg.generate_non_enw_enrolment TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_enrolment_write_pkg TO "app_farms_rest_proxy";
