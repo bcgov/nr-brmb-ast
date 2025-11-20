a9427fbe287f109ab1deb9b40b43babe999fd625
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-20 11:09:54 -0800

    AS-3349: Implement farms_chefs_pkg.update_submission.

--- farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.create_submission TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.update_submission TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_chefs_pkg TO "app_farms_rest_proxy";
