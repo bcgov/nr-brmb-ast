3c8d29d3c23f5b569634215e4eb4d3da726f60a8
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-20 11:13:56 -0800

    AS-3349: Implement farms_chefs_pkg.delete_submission.

--- farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
@@ -1,4 +1,5 @@
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.create_submission TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.update_submission TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.delete_submission TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_chefs_pkg TO "app_farms_rest_proxy";
