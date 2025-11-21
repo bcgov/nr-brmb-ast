0d5ca9971b99b9c7dd4e3af1c73422c9d647a150
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-21 09:16:56 -0800

    AS-3349: Implement farms_chefs_pkg.add_income_expense.

--- farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
@@ -5,5 +5,6 @@ GRANT EXECUTE ON FUNCTION farms_chefs_pkg.read_submissions_by_form_type TO "app_
 GRANT EXECUTE ON FUNCTION farms_chefs_pkg.read_submissions_by_guid TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.update_scenario_submission_id TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.add_puc TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.add_income_expense TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_chefs_pkg TO "app_farms_rest_proxy";
