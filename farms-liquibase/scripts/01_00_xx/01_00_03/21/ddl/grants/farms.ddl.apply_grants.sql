c1319cacce750aab57e936d063c9da47f36f40de
Hong-Yi Wang <hwang@vividsolutions.com>
2025-11-21 09:13:33 -0800

    AS-3349: Implement farms_chefs_pkg.add_puc.

--- farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
+++ farms-liquibase/scripts/01_00_xx/01_00_03/21/ddl/grants/farms.ddl.apply_grants.sql
@@ -4,5 +4,6 @@ GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.delete_submission TO "app_farms_rest_
 GRANT EXECUTE ON FUNCTION farms_chefs_pkg.read_submissions_by_form_type TO "app_farms_rest_proxy";
 GRANT EXECUTE ON FUNCTION farms_chefs_pkg.read_submissions_by_guid TO "app_farms_rest_proxy";
 GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.update_scenario_submission_id TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_chefs_pkg.add_puc TO "app_farms_rest_proxy";
 
 GRANT USAGE ON SCHEMA farms_chefs_pkg TO "app_farms_rest_proxy";
