461891fad26cbabdc72099b457a088e81b201319
Hong-Yi Wang <hwang@vividsolutions.com>
2026-01-16 15:50:40 -0800

    Implement farms_search_pkg grants.

---
+++ farms-liquibase/database/farms_search_pkg/grants.sql
@@ -0,0 +1,11 @@
+-- schema
+GRANT ALL ON SCHEMA "farms_search_pkg" TO "app_farms";
+GRANT ALL ON SCHEMA "farms_search_pkg" TO postgres;
+GRANT USAGE ON SCHEMA "farms_search_pkg" TO "app_farms_rest_proxy";
+
+-- functions
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_accounts_by_name TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_accounts_by_pin TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_accounts_by_rep TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_accounts TO "app_farms_rest_proxy";
+GRANT EXECUTE ON FUNCTION farms_search_pkg.search_imports TO "app_farms_rest_proxy";
