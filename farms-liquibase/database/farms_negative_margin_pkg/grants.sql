c0ad51882becbf49fd2d4724cce1be9fcf44df15
Hong-Yi Wang <hwang@vividsolutions.com>
2026-01-16 14:39:04 -0800

    Implement farms_negative_margin_pkg grants.

---
+++ farms-liquibase/database/farms_negative_margin_pkg/grants.sql
@@ -0,0 +1,11 @@
+-- schema
+GRANT ALL ON SCHEMA "farms_negative_margin_pkg" TO "app_farms";
+GRANT ALL ON SCHEMA "farms_negative_margin_pkg" TO postgres;
+GRANT USAGE ON SCHEMA "farms_negative_margin_pkg" TO "app_farms_rest_proxy";
+
+-- functions
+GRANT EXECUTE ON FUNCTION farms_negative_margin_pkg.get_negative_margins TO "app_farms_rest_proxy";
+
+-- procedures
+GRANT EXECUTE ON PROCEDURE farms_negative_margin_pkg.calculate_negative_margins TO "app_farms_rest_proxy";
+GRANT EXECUTE ON PROCEDURE farms_negative_margin_pkg.update_negative_margin TO "app_farms_rest_proxy";
