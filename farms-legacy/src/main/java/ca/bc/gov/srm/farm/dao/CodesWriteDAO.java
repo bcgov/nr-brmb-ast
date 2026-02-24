a4cbe2f8f6eff12263e9fd732e16c407f59a5520
Hong-Yi Wang <hwang@vividsolutions.com>
2026-02-24 12:17:14 -0800

    AS-3220: Fix CodesWriteDAO.

--- farms-legacy/src/main/java/ca/bc/gov/srm/farm/dao/CodesWriteDAO.java
+++ farms-legacy/src/main/java/ca/bc/gov/srm/farm/dao/CodesWriteDAO.java
@@ -443,13 +443,13 @@ public class CodesWriteDAO extends OracleDAO {
       connection.setAutoCommit(false);
 
       proc = new DAOStoredProcedure(connection, PACKAGE_NAME + "."
-          + procName, IN_USE_GENERIC_CODE_PARAM, Types.INTEGER);
+          + procName, IN_USE_GENERIC_CODE_PARAM, Types.NUMERIC);
       
       int param = 1;
       proc.setString(param++, code);
       proc.execute();
 
-      inUseInt = proc.getInt(1);
+      inUseInt = proc.getIntObj(1);
       result = inUseInt == 1;
 
       connection.commit();
