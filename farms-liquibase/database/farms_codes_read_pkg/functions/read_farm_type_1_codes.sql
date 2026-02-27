5a2c5c3dd539319c7053bf51c2905cec413fc362
Hong-Yi Wang <hwang@vividsolutions.com>
2026-02-27 11:12:00 -0800

    AS-3220: Fix farms_codes_read_pkg.read_farm_type_1_codes.

--- farms-liquibase/database/farms_codes_read_pkg/functions/read_farm_type_1_codes.sql
+++ farms-liquibase/database/farms_codes_read_pkg/functions/read_farm_type_1_codes.sql
@@ -17,7 +17,7 @@ begin
                tft3l.farm_type_3_name
         from farms.farm_tip_farm_type_1_lookups t
         join farms.farm_tip_farm_type_2_lookups tft2l on tft2l.tip_farm_type_2_lookup_id = t.tip_farm_type_2_lookup_id
-        join farms.farm_tip_farm_type_3_lookups tft3l on tft3l.tip_farm_type_3_lookup_id = t.tip_farm_type_3_lookup_id
+        join farms.farm_tip_farm_type_3_lookups tft3l on tft3l.tip_farm_type_3_lookup_id = tft2l.tip_farm_type_3_lookup_id
         order by lower(t.farm_type_1_name);
     return cur;
 
