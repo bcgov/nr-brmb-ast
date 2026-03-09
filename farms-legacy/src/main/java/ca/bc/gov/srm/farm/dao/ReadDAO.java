a3b4faa44b27aeeea8d79dfcc54984d29a326d13
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-09 10:17:39 -0700

    AS-3651: Fix ReadDAO.readWholeFarmParticipant.

--- farms-legacy/src/main/java/ca/bc/gov/srm/farm/dao/ReadDAO.java
+++ farms-legacy/src/main/java/ca/bc/gov/srm/farm/dao/ReadDAO.java
@@ -100,7 +100,6 @@ public class ReadDAO {
   private static final String READ_VERIFICATION_NOTES = "READ_VERIFICATION_NOTES";
 
   private static final String READ_WHOLE_FARM_PROC = "READ_PYV_WF";
-  private static final int READ_WHOLE_FARM_PARAM = 1;
 
   private static final String READ_CLAIM_PROC = "READ_SC_CLM";
   
@@ -1024,8 +1023,9 @@ public class ReadDAO {
   @SuppressWarnings("resource")
   public final HashMap<Integer, List<WholeFarmParticipant>> readWholeFarmParticipant(
       final Integer[] programYearVersions) throws SQLException {
-
-    DAOStoredProcedure proc = null;
+    long startTime = 0;
+    String prcName = PACKAGE_NAME + "." + READ_WHOLE_FARM_PROC;
+    PreparedStatement ps = null;
     ResultSet rs = null;
     boolean originalAutoCommit = true;
 
@@ -1033,15 +1033,15 @@ public class ReadDAO {
       originalAutoCommit = conn.getAutoCommit();
       conn.setAutoCommit(false);
 
-      proc = new DAOStoredProcedure(conn, PACKAGE_NAME + "."
-          + READ_WHOLE_FARM_PROC, READ_WHOLE_FARM_PARAM, true);
+      startTime = System.currentTimeMillis();
+      String sql = "SELECT * FROM " + prcName + "(?)";
+      ps = conn.prepareStatement(sql);
 
       int c = 1;
-      Array oracleArray = createNumbersOracleArray(programYearVersions);
-      proc.setArray(c++, oracleArray);
-      proc.execute();
+      Array oracleArray = createIntegersOracleArray(programYearVersions);
+      ps.setArray(c++, oracleArray);
 
-      rs = proc.getResultSet();
+      rs = ps.executeQuery();
 
       HashMap<Integer, List<WholeFarmParticipant>> r = new HashMap<>();
 
@@ -1071,9 +1071,10 @@ public class ReadDAO {
       conn.rollback();
       throw ex;
     } finally {
-
-    	close(rs, proc);
+    	close(rs, ps);
       conn.setAutoCommit(originalAutoCommit);
+      long duration = System.currentTimeMillis() - startTime;
+      logger.debug("{} took {} ms", prcName, duration);
     }
   }
 
