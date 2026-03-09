0f2459d2896eb4fb1952a8447b300fcb9a87c785
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-09 16:55:43 -0700

    AS-3651: Fix farms_read_pkg.read_op_fmv_prev_year.

--- farms-liquibase/database/farms_read_pkg/functions/read_op_fmv_prev_year.sql
+++ farms-liquibase/database/farms_read_pkg/functions/read_op_fmv_prev_year.sql
@@ -12,32 +12,33 @@ as $$
            g.crop_unit_code,
            g.prev_fy_end_average_price
     from (
-        select prev_fy_fmv.inventory_item_code,
-               prev_fy_fmv.crop_unit_code,
-               prev_fy_fmv.municipality_code,
-               max(prev_fy_fmv.municipality_code) over (partition by prev_fy_fmv.inventory_item_code, prev_fy_fmv.crop_unit_code, prev_fy_fmv.program_year, prev_fy_fmv.period) mx_municipality_code,
-               prev_fy_fmv.average_price prev_fy_end_average_price
+        select t.inventory_item_code,
+               t.crop_unit_code,
+               t.municipality_code,
+               max(t.municipality_code) over (partition by t.inventory_item_code, t.crop_unit_code, t.program_year, t.period) mx_municipality_code,
+               t.average_price prev_fy_end_average_price
         from (
-            select distinct extract(year from op.fiscal_year_start - interval '1 month') prev_fiscal_end_year,
-                   extract(month from op.fiscal_year_start - interval '1 month') prev_fiscal_end_month,
-                   pyv.municipality_code,
+            select distinct pyv.municipality_code,
                    x.inventory_item_code,
                    x.inventory_class_code,
-                   (case when x.inventory_class_code = '1' then inv.crop_unit_code end) crop_unit_code
+                   (case when x.inventory_class_code = '1' then inv.crop_unit_code end) crop_unit_code,
+                   fmv.average_price,
+                   fmv.program_year,
+                   fmv.period,
+                   fmv.expiry_date
             from farms.farm_farming_operations op
             join farms.farm_program_year_versions pyv on op.program_year_version_id = pyv.program_year_version_id
             join farms.farm_reported_inventories inv on op.farming_operation_id = inv.farming_operation_id
             join farms.farm_agristabilty_cmmdty_xref x on inv.agristabilty_cmmdty_xref_id = x.agristabilty_cmmdty_xref_id
+            join farms.farm_fair_market_values fmv on fmv.inventory_item_code = x.inventory_item_code
+                                                    -- either the crop code matches, or it's livestock
+                                                    and ((x.inventory_class_code = '1' and fmv.crop_unit_code = inv.crop_unit_code) or x.inventory_class_code = '2')
+                                                    and fmv.municipality_code in ('0', pyv.municipality_code)
+                                                    and fmv.program_year = extract(year from op.fiscal_year_start - interval '1 month')
+                                                    and fmv.period = extract(month from op.fiscal_year_start - interval '1 month')
             where op.farming_operation_id = op_id
         ) t
-        join farms.farm_fair_market_values prev_fy_fmv on prev_fy_fmv.inventory_item_code = t.inventory_item_code
-                                                    -- either the crop code matches, or it's livestock
-                                                    and ((t.inventory_class_code = '1' and prev_fy_fmv.crop_unit_code = t.crop_unit_code) or t.inventory_class_code = '2')
-                                                    and prev_fy_fmv.municipality_code in ('0', t.municipality_code)
-                                                    and coalesce(prev_fy_fmv.expiry_date, '9999-12-31') > current_date
-                                                    and prev_fy_fmv.program_year = t.prev_fiscal_end_year
-                                                    and prev_fy_fmv.period = t.prev_fiscal_end_month
-        where prev_fy_fmv.program_year = prev_fiscal_end_year
+        where coalesce(t.expiry_date, '9999-12-31') > current_date
     ) g
     where g.municipality_code = mx_municipality_code
     order by g.inventory_item_code,
