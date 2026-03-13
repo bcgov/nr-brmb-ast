fc2ff51e819ab775fa92997b1d87db4c116be047
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-13 14:23:58 -0700

    AS-3651: Fix farms_calculator_pkg.read_all_partners.

--- farms-liquibase/database/farms_calculator_pkg/functions/read_all_partners.sql
+++ farms-liquibase/database/farms_calculator_pkg/functions/read_all_partners.sql
@@ -17,7 +17,7 @@ as $$
     join farms.farm_program_year_versions pyv on pyv.program_year_id = py.program_year_id
     join farms.farm_farming_operations fo on fo.program_year_version_id = pyv.program_year_version_id
     join farms.farm_farming_operatin_prtnrs fop on fop.farming_operation_id = fo.farming_operation_id
-    where py.year >= (to_char(current_timestamp, 'YYYY')::int - 6) -- only check current program year and its reference years
+    where py.year >= (extract(year from current_timestamp) - 6) -- only check current program year and its reference years
     order by fop.partnership_pin,
              corp_name,
              last_name,
