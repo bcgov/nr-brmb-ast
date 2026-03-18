abc12fc767b8d818cde454acdd43e2dce1882e7b
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-18 09:01:39 -0700

    AS-3217: Increase the size of user ID columns to accommodate email address.

---
+++ farms-liquibase/releases/01_00_xx/01_00_00/00/farms/ddl/users.sql
@@ -0,0 +1,544 @@
+ALTER TABLE farms.farm_aarm_margins
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_agristability_claims
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_agristability_clients
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_agristability_represntves
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_agristability_scenarios
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_agristabilty_cmmdty_xref
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_benchmark_per_units
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_benchmark_years
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_benefit_calc_documents
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_benefit_calc_margins
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_benefit_calc_totals
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_chef_form_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_chef_submissions
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_chef_submssn_crm_entities
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_chef_submssn_status_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_client_subscriptions
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_commodity_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_config_param_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_configuration_parameters
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_crm_entity_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_crop_unit_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_crop_unit_conversn_fctrs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_crop_unit_defaults
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_document_templates
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_enrolment_calc_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_expected_productions
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_fair_market_values
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_farm_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_farming_operatin_prtnrs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_farming_operations
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_federal_accounting_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_federal_status_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_fruit_veg_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_fruit_veg_type_details
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_import_class_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_import_logs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_import_state_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_import_versions
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_inventory_class_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_inventory_group_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_inventory_item_attributes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_inventory_item_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_inventory_item_details
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_line_items
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_market_rate_premium
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_message_type_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_multi_stage_commdty_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_municipality_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_negative_margins
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_office_municipality_xref
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_participant_class_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_participant_lang_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_participant_profile_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_participnt_data_src_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_persons
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_production_insurances
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_productve_unit_capacities
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_program_enrolments
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_program_year_versions
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_program_years
+ALTER COLUMN assigned_to_userid TYPE varchar(256),
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_reasonabilty_test_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_reference_scenarios
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_regional_office_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_reported_income_expenses
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_reported_inventories
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_bnft_rsk_prd_un_rslts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_forage_consumers
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_prdctn_forage_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_prdctn_frut_invntries
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_prdctn_frut_veg_rslts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_prdctn_grain_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_result_messages
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_fruit_veg_invntrs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_fruit_veg_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_g_f_fs_incm_rslts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_g_f_fs_invntories
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_hog_inventories
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_hog_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_nursery_incomes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_nursery_invntries
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_nursery_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_poultry_brl_rslts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_rsn_rev_poultry_egg_rslts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_bpu_purpos_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_bpu_xref
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_category_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_class_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_config_params
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_enrolments
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_logs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_state_audits
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_scenario_state_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_sector_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_sector_detail_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_sector_detail_line_items
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_sector_detail_xref
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_structural_change_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_structure_group_attributs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_structure_group_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_subscription_status_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_benchmark_expenses
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_benchmark_years
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_farm_type_1_lookups
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_farm_type_2_lookups
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_farm_type_3_lookups
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_income_ranges
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_line_items
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_rating_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_report_documents
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_report_expenses
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_tip_report_results
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_triage_queue_codes
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_urls
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_users
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_whole_farm_participants
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_year_configuration_params
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z01_participant_infos
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z02_partpnt_farm_infos
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z03_statement_infos
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z04_income_exps_dtls
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z05_partner_infos
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z21_participant_suppls
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z22_production_insurances
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z23_livestock_prod_cpcts
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z28_prod_insurance_refs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z29_inventory_code_refs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z40_prtcpnt_ref_supl_dtls
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z42_participant_ref_years
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z50_participnt_bnft_calcs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z51_participant_contribs
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_z99_extract_file_ctls
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_zaarm_margins
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_zbpu_benchmark_per_units
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_zfmv_fair_market_values
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_zivpr_iv_premium_rates
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
+
+ALTER TABLE farms.farm_zprogram_enrolments
+ALTER COLUMN who_created TYPE varchar(256),
+ALTER COLUMN who_updated TYPE varchar(256);
