ec530bbd9383d606b8658528585cb254f75358f2
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-20 10:03:42 -0700

    Implement BenchmarkPerUnitEntity.

---
+++ farms-api-new/src/main/java/ca/bc/gov/farms/data/entities/BenchmarkPerUnitEntity.java
@@ -0,0 +1,53 @@
+package ca.bc.gov.farms.data.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BenchmarkPerUnitEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long benchmarkPerUnitId;
+    private Integer programYear;
+    private String unitComment;
+    private Date expiryDate;
+    private String municipalityCode;
+    private String municipalityDesc;
+    private String inventoryItemCode;
+    private String inventoryItemDesc;
+    private String structureGroupCode;
+    private String structureGroupDesc;
+    private String inventoryCode;
+    private String inventoryDesc;
+    private BigDecimal yearMinus6Margin;
+    private BigDecimal yearMinus5Margin;
+    private BigDecimal yearMinus4Margin;
+    private BigDecimal yearMinus3Margin;
+    private BigDecimal yearMinus2Margin;
+    private BigDecimal yearMinus1Margin;
+    private BigDecimal yearMinus6Expense;
+    private BigDecimal yearMinus5Expense;
+    private BigDecimal yearMinus4Expense;
+    private BigDecimal yearMinus3Expense;
+    private BigDecimal yearMinus2Expense;
+    private BigDecimal yearMinus1Expense;
+    private Long urlId;
+    private String url;
+
+    private Integer revisionCount;
+    private String createUser;
+    private Date createDate;
+    private String updateUser;
+    private Date updateDate;
+}
