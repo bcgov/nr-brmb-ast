48eaed7310e879f8a6319c3dc3732aee5def3c10
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-24 16:10:36 -0700

    Implement FairMarketValueMapper.

--- farms-api/src/main/java/ca/bc/gov/farms/data/mappers/FairMarketValueMapper.java
+++ farms-api/src/main/java/ca/bc/gov/farms/data/mappers/FairMarketValueMapper.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.farms.data.mappers;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -15,9 +16,11 @@ public interface FairMarketValueMapper {
     List<FairMarketValueEntity> fetchBy(Integer programYear, String inventoryItemCode, String municipalityCode,
             String cropUnitCode);
 
-    int insertFairMarketValue(FairMarketValueEntity dto, String userId);
+    int insertFairMarketValue(FairMarketValueEntity dto, String userId, Integer period, BigDecimal averagePrice,
+            BigDecimal percentVariance);
 
-    int updateFairMarketValue(FairMarketValueEntity dto, String userId);
+    int updateFairMarketValue(FairMarketValueEntity dto, Long urlId, String userId, Integer period,
+            BigDecimal averagePrice, BigDecimal percentVariance);
 
     int deleteFairMarketValue(Integer programYear, String fairMarketValueId);
 }
