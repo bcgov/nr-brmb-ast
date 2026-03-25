35fe346e381effd66e79155c4875a875f13c179c
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-25 15:13:48 -0700

    Implement InventoryTypeXrefEntity.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/entities/InventoryTypeXrefEntity.java
@@ -0,0 +1,34 @@
+package ca.bc.gov.farms.data.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryTypeXrefEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long agristabilityCommodityXrefId;
+    private String marketCommodityInd;
+    private String inventoryItemCode;
+    private String inventoryItemDesc;
+    private String inventoryGroupCode;
+    private String inventoryGroupDesc;
+    private String inventoryClassCode;
+    private String inventoryClassDesc;
+
+    private Integer revisionCount;
+    private String createUser;
+    private Date createDate;
+    private String updateUser;
+    private Date updateDate;
+}
