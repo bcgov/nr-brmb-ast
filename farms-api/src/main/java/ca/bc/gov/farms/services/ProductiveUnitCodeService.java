54cdfb7a05462b9990174aa8c4ce7d4c858f024f
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-30 13:11:04 -0700

    Implement ProductiveUnitCodeService.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/services/ProductiveUnitCodeService.java
@@ -0,0 +1,39 @@
+package ca.bc.gov.farms.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.brmb.common.service.api.ServiceException;
+import ca.bc.gov.farms.data.assemblers.ProductiveUnitCodeResourceAssembler;
+import ca.bc.gov.farms.data.entities.ProductiveUnitCodeEntity;
+import ca.bc.gov.farms.data.mappers.ProductiveUnitCodeMapper;
+import ca.bc.gov.farms.data.models.ProductiveUnitCodeListModel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ProductiveUnitCodeService {
+
+    @Autowired
+    private ProductiveUnitCodeMapper productiveUnitCodeMapper;
+
+    @Autowired
+    private ProductiveUnitCodeResourceAssembler productiveUnitCodeResourceAssembler;
+
+    public ProductiveUnitCodeListModel getAllProductiveUnitCodes() throws ServiceException {
+
+        ProductiveUnitCodeListModel result = null;
+
+        try {
+            List<ProductiveUnitCodeEntity> entities = productiveUnitCodeMapper.fetchAll();
+
+            result = productiveUnitCodeResourceAssembler.getProductiveUnitCodeList(entities);
+        } catch (Throwable t) {
+            throw new ServiceException("Mapper threw an exception", t);
+        }
+
+        return result;
+    }
+}
