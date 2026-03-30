8639f8f11b517042e35db4470b89e3632988920b
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-30 09:37:04 -0700

    Implement MarketRatePremiumEntity.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/entities/MarketRatePremiumEntity.java
@@ -0,0 +1,33 @@
+package ca.bc.gov.farms.data.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarketRatePremiumEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long marketRatePremiumId;
+    private BigDecimal minTotalPremiumAmount;
+    private BigDecimal maxTotalPremiumAmount;
+    private BigDecimal riskChargeFlatAmount;
+    private BigDecimal riskChargePctPremium;
+    private BigDecimal adjustChargeFlatAmount;
+
+    private Integer revisionCount;
+    private String createUser;
+    private Date createDate;
+    private String updateUser;
+    private Date updateDate;
+}
