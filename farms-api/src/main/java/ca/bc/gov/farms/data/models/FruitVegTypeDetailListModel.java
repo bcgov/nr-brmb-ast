174af604fb1546eb79ac31d26d0345a4e5e3bd55
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-29 18:22:08 -0700

    Implement FruitVegTypeDetailListModel.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/models/FruitVegTypeDetailListModel.java
@@ -0,0 +1,24 @@
+package ca.bc.gov.farms.data.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import ca.bc.gov.brmb.common.rest.resource.BaseResource;
+import ca.bc.gov.farms.data.entities.FruitVegTypeDetailEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FruitVegTypeDetailListModel extends BaseResource {
+
+    private List<FruitVegTypeDetailEntity> fruitVegTypeDetailList;
+}
