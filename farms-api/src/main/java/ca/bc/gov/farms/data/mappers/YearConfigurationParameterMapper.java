c157c7a15fa42baa8fa955a60d2086687a0ac99a
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-29 19:12:19 -0700

    Implement YearConfigurationParameterMapper.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/mappers/YearConfigurationParameterMapper.java
@@ -0,0 +1,22 @@
+package ca.bc.gov.farms.data.mappers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ca.bc.gov.farms.data.entities.YearConfigurationParameterEntity;
+
+@Mapper
+public interface YearConfigurationParameterMapper {
+
+    YearConfigurationParameterEntity fetch(Long yearConfigurationParameterId);
+
+    List<YearConfigurationParameterEntity> fetchAll();
+
+    int insertYearConfigurationParameter(YearConfigurationParameterEntity dto, String userId);
+
+    int updateYearConfigurationParameter(YearConfigurationParameterEntity dto, String userId);
+
+    int deleteYearConfigurationParameter(Long yearConfigurationParameterId);
+}
