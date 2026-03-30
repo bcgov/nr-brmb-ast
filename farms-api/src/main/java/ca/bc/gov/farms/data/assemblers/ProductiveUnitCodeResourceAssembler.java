d1145edc1772a9243b5e0dce47018e20b7249a33
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-30 13:07:55 -0700

    Implement ProductiveUnitCodeResourceAssembler.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/assemblers/ProductiveUnitCodeResourceAssembler.java
@@ -0,0 +1,44 @@
+package ca.bc.gov.farms.data.assemblers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.farms.data.entities.ProductiveUnitCodeEntity;
+import ca.bc.gov.farms.data.models.ProductiveUnitCodeListModel;
+import ca.bc.gov.farms.data.models.ProductiveUnitCodeModel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class ProductiveUnitCodeResourceAssembler extends BaseResourceAssembler {
+
+    public ProductiveUnitCodeListModel getProductiveUnitCodeList(List<ProductiveUnitCodeEntity> entities) {
+
+        URI baseUri = getBaseURI();
+
+        ProductiveUnitCodeListModel result = null;
+
+        @SuppressWarnings("null")
+        List<ProductiveUnitCodeModel> resources = entities.stream().filter(Objects::nonNull).map(entity -> {
+            ProductiveUnitCodeModel resource = new ProductiveUnitCodeModel();
+            BeanUtils.copyProperties(entity, resource);
+            setSelfLink(entity.getCode(), resource, baseUri);
+            return resource;
+        }).collect(Collectors.toList());
+
+        result = new ProductiveUnitCodeListModel();
+        result.setProductiveUnitCodeList(resources);
+
+        String eTag = getEtag(result);
+        result.setETag(eTag);
+
+        setSelfLink(result, baseUri);
+
+        return result;
+    }
+}
