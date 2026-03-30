b20f2e33b149af55357417c0337efacc904fa270
Hong-Yi Wang <hwang@vividsolutions.com>
2026-03-30 10:56:15 -0700

    Implement CropUnitConversionMapper.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/mappers/CropUnitConversionMapper.java
@@ -0,0 +1,26 @@
+package ca.bc.gov.farms.data.mappers;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+import ca.bc.gov.farms.data.entities.ConversionUnitEntity;
+import ca.bc.gov.farms.data.entities.CropUnitConversionEntity;
+
+@Mapper
+public interface CropUnitConversionMapper {
+
+    CropUnitConversionEntity fetch(Long cropUnitDefaultId);
+
+    List<CropUnitConversionEntity> fetchAll();
+
+    List<CropUnitConversionEntity> fetchByInventoryItemCode(String inventoryItemCode);
+
+    int insertCropUnitDefault(CropUnitConversionEntity dto, String userId);
+    int insertCropUnitConversionFactor(ConversionUnitEntity dto, String userId);
+
+    int updateCropUnitDefault(CropUnitConversionEntity dto, String userId);
+    int updateCropUnitConversionFactor(ConversionUnitEntity dto, String userId);
+
+    int deleteCropUnitDefault(Long cropUnitDefaultId);
+    int deleteCropUnitConversionFactor(Long cropUnitConversionFactorId);
+}
