85214cff52fad5e0d9f5ef66d382b6b0c7498657
Hong-Yi Wang <hwang@vividsolutions.com>
2026-04-02 12:01:30 -0700

    Implement CropUnitConversionControllerTest.

--- farms-api/src/test/resources/farms/tables/data.sql
+++ farms-api/src/test/resources/farms/tables/data.sql
@@ -327,26 +327,6 @@ insert into farms.farm_fruit_veg_type_codes (
     current_timestamp
 );
 
-insert into farms.farm_crop_unit_defaults (
-    crop_unit_default_id,
-    inventory_item_code,
-    crop_unit_code,
-    revision_count,
-    who_created,
-    when_created,
-    who_updated,
-    when_updated
-) values (
-    nextval('farms.farm_cud_seq'),
-    '5560',
-    '2',
-    1,
-    current_user,
-    current_timestamp,
-    current_user,
-    current_timestamp
-);
-
 insert into farms.farm_urls (
     url_id,
     url,
