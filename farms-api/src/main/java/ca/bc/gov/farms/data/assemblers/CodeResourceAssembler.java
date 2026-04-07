9987bbeade8f7835b489c5c2d61dc5ecadfc7608
Hong-Yi Wang <hwang@vividsolutions.com>
2026-04-06 18:53:23 -0700

    Implement CodeResourceAssembler.

---
+++ farms-api/src/main/java/ca/bc/gov/farms/data/assemblers/CodeResourceAssembler.java
@@ -0,0 +1,36 @@
+package ca.bc.gov.farms.data.assemblers;
+
+import java.net.URI;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.farms.data.entities.CodeEntity;
+import ca.bc.gov.farms.data.models.CodeModel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CodeResourceAssembler extends BaseResourceAssembler {
+
+    public CodeModel getCode(@NonNull CodeEntity entity) {
+
+        URI baseUri = getBaseURI();
+
+        CodeModel resource = new CodeModel();
+
+        BeanUtils.copyProperties(entity, resource);
+
+        String eTag = getEtag(resource);
+        resource.setETag(eTag);
+
+        setSelfLink(resource.getCode(), resource, baseUri);
+
+        return resource;
+    }
+
+    public void updateCode(@NonNull CodeModel resource, @NonNull CodeEntity entity) {
+        BeanUtils.copyProperties(resource, entity);
+    }
+}
