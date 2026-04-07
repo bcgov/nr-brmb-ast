8666bffc60b5375f9d99bca93c2ee85d0d0bd2ea
Hong-Yi Wang <hwang@vividsolutions.com>
2026-04-07 13:53:00 -0700

    Implement CodeTableControllerTest.

--- farms-api/src/test/java/ca/bc/gov/farms/controllers/CodeTableControllerTest.java
+++ farms-api/src/test/java/ca/bc/gov/farms/controllers/CodeTableControllerTest.java
@@ -1,6 +1,7 @@
 package ca.bc.gov.farms.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
@@ -26,13 +27,23 @@ public class CodeTableControllerTest {
 
     private static final String TABLE_NAME = "farm_municipality_codes";
     private static final LocalDate EFFECTIVE_DATE = LocalDate.now();
-    private static final LocalDate EXPIRY_DATE = EFFECTIVE_DATE.plusYears(1);
 
     @Test
     @Order(1)
     public void testGetCodeTable() throws Exception {
 
         mockMvc.perform(get("/codeTables/" + TABLE_NAME))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.@type").value("CodeTableModel"))
+                .andExpect(jsonPath("$.codeTableName").value(TABLE_NAME))
+                .andExpect(jsonPath("$.codeTableDescriptiveName").value(TABLE_NAME))
+                .andExpect(jsonPath("$.codes[0].code").value("41"))
+                .andExpect(jsonPath("$.codes[0].description").value("Cariboo"))
+                .andExpect(jsonPath("$.codes[0].effectiveDate").value(EFFECTIVE_DATE.toString()))
+                .andExpect(jsonPath("$.codes[0].expiryDate").value("9999-12-31"))
+                .andExpect(jsonPath("$.codes[1].code").value("43"))
+                .andExpect(jsonPath("$.codes[1].description").value("Mount Waddington (Island part)"))
+                .andExpect(jsonPath("$.codes[1].effectiveDate").value(EFFECTIVE_DATE.toString()))
+                .andExpect(jsonPath("$.codes[1].expiryDate").value("9999-12-31"));
     }
 }
