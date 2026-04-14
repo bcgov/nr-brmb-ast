2d9bcef548f658fc7aab5d7f7b96cb35a507433b
Hong-Yi Wang <hwang@vividsolutions.com>
2026-04-14 11:12:13 -0700

    AS-3689: Create the Generic Codes collection in Postman with test cases.

--- farms-api/src/main/java/ca/bc/gov/farms/data/models/CodeRsrc.java
+++ farms-api/src/main/java/ca/bc/gov/farms/data/models/CodeRsrc.java
@@ -2,15 +2,21 @@ package ca.bc.gov.farms.data.models;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import ca.bc.gov.brmb.common.rest.resource.BaseResource;
+import ca.bc.gov.brmb.common.rest.resource.types.BaseResourceTypes;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@XmlRootElement(namespace = BaseResourceTypes.COMMON_NAMESPACE, name = BaseResourceTypes.CODE_NAME)
+@JsonSubTypes({ @Type(value = CodeRsrc.class, name = BaseResourceTypes.CODE) })
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 @Data
 @EqualsAndHashCode(callSuper = false)
