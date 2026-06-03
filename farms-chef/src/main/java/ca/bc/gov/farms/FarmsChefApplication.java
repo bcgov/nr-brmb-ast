ec347fde13878ac956ac42672ac0703e84172e0f
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-03 14:01:36 -0700

    AS-3748: Pretty print JSON.

--- farms-chef/src/main/java/ca/bc/gov/farms/FarmsChefApplication.java
+++ farms-chef/src/main/java/ca/bc/gov/farms/FarmsChefApplication.java
@@ -1,12 +1,7 @@
 package ca.bc.gov.farms;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.core.command.annotation.Command;
-import org.springframework.shell.core.command.annotation.Option;
 
 @SpringBootApplication
 public class FarmsChefApplication {
@@ -15,8 +10,4 @@ public class FarmsChefApplication {
 		SpringApplication.run(FarmsChefApplication.class, args);
 	}
 
-	@Command(name = "print")
-	public String print(@Option String jsonFilePath) throws Exception {
-		return Files.readString(Path.of(jsonFilePath));
-	}
 }
---
+++ farms-chef/src/main/java/ca/bc/gov/farms/JsonCommands.java
@@ -0,0 +1,29 @@
+package ca.bc.gov.farms;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.shell.core.command.annotation.Command;
+import org.springframework.shell.core.command.annotation.Option;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class JsonCommands {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Command(name = "print")
+    public String print(@Option String jsonFilePath) {
+        try {
+            String rawJson = Files.readString(Path.of(jsonFilePath));
+
+            Object jsonObject = objectMapper.readValue(rawJson, Object.class);
+
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+        } catch (Exception e) {
+            return "Invalid JSON: " + e.getMessage();
+        }
+    }
+}
