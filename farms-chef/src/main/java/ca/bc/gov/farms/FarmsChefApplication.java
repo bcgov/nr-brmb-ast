19bc2776e74c4a781fc715b6c9730786a5c6ff03
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-03 13:50:34 -0700

    AS-3748: Print JSON file.

--- farms-chef/src/main/java/ca/bc/gov/farms/FarmsChefApplication.java
+++ farms-chef/src/main/java/ca/bc/gov/farms/FarmsChefApplication.java
@@ -1,7 +1,12 @@
 package ca.bc.gov.farms;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.core.command.annotation.Command;
+import org.springframework.shell.core.command.annotation.Option;
 
 @SpringBootApplication
 public class FarmsChefApplication {
@@ -10,4 +15,8 @@ public class FarmsChefApplication {
 		SpringApplication.run(FarmsChefApplication.class, args);
 	}
 
+	@Command(name = "print")
+	public String print(@Option String jsonFilePath) throws Exception {
+		return Files.readString(Path.of(jsonFilePath));
+	}
 }
