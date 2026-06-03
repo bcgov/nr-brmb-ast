5c05ff379b9d25ab5ed6e0dd8897f46b08e37b91
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-03 13:23:49 -0700

    AS-3748: Add farms-chef.

---
+++ farms-chef/src/test/java/ca/bc/gov/farms/FarmsChefApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.farms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmsChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
