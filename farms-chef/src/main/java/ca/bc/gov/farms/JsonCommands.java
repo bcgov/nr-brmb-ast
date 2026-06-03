9a71ad4fe1913a3b8d856c6a8d40ecee29665688
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-03 14:16:47 -0700

    AS-3748: Filter out the id key.

--- farms-chef/src/main/java/ca/bc/gov/farms/JsonCommands.java
+++ farms-chef/src/main/java/ca/bc/gov/farms/JsonCommands.java
@@ -2,17 +2,27 @@ package ca.bc.gov.farms;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.springframework.shell.core.command.annotation.Command;
 import org.springframework.shell.core.command.annotation.Option;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+@JsonIgnoreProperties({ "id" })
+abstract class IdMixin {
+}
 
 @Component
 public class JsonCommands {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonCommands() {
+        objectMapper.addMixIn(Map.class, IdMixin.class);
+    }
 
     @Command(name = "print")
     public String print(@Option String jsonFilePath) {
