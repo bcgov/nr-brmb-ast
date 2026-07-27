11d77db970aadb446a9a2e7efa1529d0f4d93430
Andrew Wilkinson <Andrew.J.Wilkinson@gov.bc.ca>
2026-07-27 15:20:12 -0700

    Fix contact transfer: corrected farms_import_pkg.contact_transfer parameter type

--- farms-liquibase/database/farms_import_pkg/procedures/contact_transfer.sql
+++ farms-liquibase/database/farms_import_pkg/procedures/contact_transfer.sql
@@ -1,5 +1,5 @@
 create or replace procedure farms_import_pkg.contact_transfer(
-   in in_cra_version_id numeric,
+   in in_cra_version_id farms.farm_import_versions.import_version_id%type,
    in in_changed_contact_client_ids numeric[],
    in in_user varchar
 )
